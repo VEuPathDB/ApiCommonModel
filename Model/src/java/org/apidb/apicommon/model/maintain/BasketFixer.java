72d96519d2e55d098a1b939fdc84c63b2c0761b1
Xin Gao <xingao@upenn.edu>
2011-02-04 01:48:09 +0000

    fix basket fixer to handle cases where new id already exists, and multiple old ids mapped to the same new id.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@38737 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/maintain/BasketFixer.java
+++ Model/src/java/org/apidb/apicommon/model/maintain/BasketFixer.java
@@ -95,20 +95,23 @@ public class BasketFixer extends BaseCLI {
         String type = "GeneRecordClasses.GeneRecordClass";
         String aliasTable = "apidb.GeneId";
         String idColumn = "gene";
-        Map<String, String> ids = getDeprecatedIds(wdkModel, type, aliasTable,
-                idColumn);
-        changeIds(wdkModel, type, ids);
+        Map<Integer, Map<String, String>> users = getDeprecatedIds(wdkModel,
+                type, aliasTable, idColumn);
+        for (int userId : users.keySet()) {
+            Map<String, String> ids = users.get(userId);
+            changeIds(wdkModel, type, userId, ids);
+        }
     }
 
-    private Map<String, String> getDeprecatedIds(WdkModel wdkModel,
-            String type, String aliasTable, String idColumn)
+    private Map<Integer, Map<String, String>> getDeprecatedIds(
+            WdkModel wdkModel, String type, String aliasTable, String idColumn)
             throws SQLException {
-        Map<String, String> ids = new HashMap<String, String>();
+        Map<Integer, Map<String, String>> users = new HashMap<Integer, Map<String, String>>();
 
         String userSchema = wdkModel.getModelConfig().getUserDB().getUserSchema();
         String dblink = wdkModel.getModelConfig().getAppDB().getUserDbLink();
         DataSource dataSource = wdkModel.getQueryPlatform().getDataSource();
-        String sql = "SELECT b.pk_column_1 AS old_id,                   "
+        String sql = "SELECT b.user_id, b.pk_column_1 AS old_id,        "
                 + "          a." + idColumn + " AS new_id               "
                 + "   FROM " + userSchema + "user_baskets" + dblink + " b "
                 + "     LEFT JOIN " + aliasTable + " a "
@@ -117,35 +120,92 @@ public class BasketFixer extends BaseCLI {
 
         ResultSet resultSet = null;
         try {
-            logger.debug("executing sql to find invalid basket ids...");
+            logger.debug("executing sql to find invalid basket ids...\n" + sql);
             PreparedStatement ps = SqlUtils.getPreparedStatement(dataSource,
                     sql);
+            ps.setFetchSize(5000);
             ps.setString(1, wdkModel.getProjectId());
             ps.setString(2, type);
             resultSet = ps.executeQuery();
             logger.debug("sql returned.");
             while (resultSet.next()) {
+                int userId = resultSet.getInt("user_id");
                 String oldId = resultSet.getString("old_id");
                 String newId = resultSet.getString("new_id");
+
+                Map<String, String> ids = users.get(userId);
+                if (ids == null) {
+                    ids = new HashMap<String, String>();
+                    users.put(userId, ids);
+                }
                 ids.put(oldId, newId);
             }
+
+            Integer[] userIds = new Integer[users.size()];
+            users.keySet().toArray(userIds);
+            for (Integer userId : userIds) {
+                Map<String, String> ids = users.get(userId);
+                // if the old id maps to a new id, but the new id is also in the
+                // basket, then need to remove the old id, by setting the new id
+                // to null.
+                String[] oldIds = new String[ids.size()];
+                ids.keySet().toArray(oldIds);
+                for (String oldId : oldIds) {
+                    String newId = ids.get(oldId);
+                    // if new id is null, meaning old id to be removed, don't
+                    // process here.
+                    if (newId == null) continue;
+
+                    // if old id == new id, don't process here. this entry will
+                    // be discarded later.
+                    if (oldId.equals(newId)) continue;
+
+                    if (ids.containsKey(newId)) {
+                        // new id already exist, remove the old id by setting
+                        // new id to null.
+                        ids.put(oldId, null);
+                    } else {
+                        // new id doesn't exist, add new id into the list, so
+                        // that other old id which maps to this new id will be
+                        // removed. However, the current old id will be mapped.
+                        ids.put(newId, newId);
+                    }
+                }
+
+                // discard entries where old id == new id; this has to be done
+                // after the previous step.
+                oldIds = new String[ids.size()];
+                ids.keySet().toArray(oldIds);
+                for (String oldId : oldIds) {
+                    String newId = ids.get(oldId);
+                    if (newId != null && oldId.equals(newId))
+                        ids.remove(oldId);
+                }
+
+                // if the user has no id to process, discard the user
+                if (ids.size() == 0) users.remove(userId);
+            }
+            logger.info("total " + users.size()
+                    + " users needs to be changed.");
         } finally {
             SqlUtils.closeResultSet(resultSet);
         }
-        return ids;
+        return users;
     }
 
-    private void changeIds(WdkModel wdkModel, String type,
+    private void changeIds(WdkModel wdkModel, String type, int userId,
             Map<String, String> ids) throws SQLException {
+        logger.info("Updating basket for user #" + userId);
+        
         String userSchema = wdkModel.getModelConfig().getUserDB().getUserSchema();
         DataSource dataSource = wdkModel.getUserPlatform().getDataSource();
         String sqlUpdate = "UPDATE " + userSchema + "user_baskets "
                 + " SET pk_column_1 = ? "
-                + " WHERE project_id = ? AND record_class = ? "
-                + "       AND pk_column_1 = ?";
+                + " WHERE project_id = ? AND user_id = ? "
+                + "   AND record_class = ? AND pk_column_1 = ?";
         String sqlDelete = "DELETE FROM " + userSchema + "user_baskets "
-                + " WHERE project_id = ? AND record_class = ? "
-                + "       AND pk_column_1 = ?";
+                + " WHERE project_id = ? AND user_id = ? "
+                + "   AND record_class = ? AND pk_column_1 = ?";
 
         PreparedStatement psUpdate = null, psDelete = null;
         try {
@@ -157,24 +217,26 @@ public class BasketFixer extends BaseCLI {
             for (String oldId : ids.keySet()) {
                 String newId = ids.get(oldId);
                 if (newId == null) {
-                    logger.debug("deleting basket id: " + oldId + " of type "
-                            + type);
+                    logger.trace("deleting user #" + userId + " basket id: "
+                            + oldId + " of type " + type);
                     psDelete.setString(1, wdkModel.getProjectId());
-                    psDelete.setString(2, type);
-                    psDelete.setString(3, oldId);
-                    psDelete.addBatch();
+                    psDelete.setInt(2, userId);
+                    psDelete.setString(3, type);
+                    psDelete.setString(4, oldId);
+                     psDelete.addBatch();
                 } else {
-                    logger.debug("change basket id: " + oldId + " to " + newId
-                            + " of type " + type);
+                    logger.trace("change user #" + userId + " basket id: "
+                            + oldId + " to " + newId + " of type " + type);
                     psUpdate.setString(1, newId);
                     psUpdate.setString(2, wdkModel.getProjectId());
-                    psUpdate.setString(3, type);
-                    psUpdate.setString(4, oldId);
-                    psUpdate.addBatch();
+                    psUpdate.setInt(3, userId);
+                    psUpdate.setString(4, type);
+                    psUpdate.setString(5, oldId);
+                     psUpdate.addBatch();
                 }
             }
-            // psUpdate.executeBatch();
-            // psDelete.executeBatch();
+             psUpdate.executeBatch();
+             psDelete.executeBatch();
         } finally {
             SqlUtils.closeStatement(psUpdate);
             SqlUtils.closeStatement(psDelete);
