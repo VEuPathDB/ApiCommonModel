22bf2c535fcbb6f359ae8546f10084c506830f2d
Ja’Shon Cade <jcade@upenn.edu>
2012-09-10 16:53:19 +0000

    New Script to that takes in the old resource.xml file, a tab file that maps old externalDatabaseNames to the new ones and the name of an output file and generates the xml nodes for the new Data source attribution file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@49928 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/bin/DataSourceUpdater.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/perl
+
+use strict;
+
+use XML::Simple;
+
+use Getopt::Long;
+
+use Data::Dumper;
+
+my ($ResourceXML, $DatasetAttributionXML, $mappingFile);
+
+GetOptions("sourceFile=s" => \$ResourceXML,
+           "outputFile=s" => \$DatasetAttributionXML,
+           "mappingFile=s" => \$mappingFile);
+
+  my $sourceData = new XML::Simple;
+  my $source = $sourceData->XMLin($ResourceXML, ForceArray => 1, KeyAttr => { resource => '+resource',  NoAttr => 0}) if ($ResourceXML);
+
+
+#   open my $fh, '>', $DatasetAttributionXML or die "open ( $DatasetAttributionXML): $!";
+#   XMLout($source,OutputFile => $fh);
+#   close $fh;
+#   print STDERR Dumper($source);
+
+
+my $usage = " usage - DataSourceUpdater.pl --sourceFile [old resource.xml file name] --outputFile [new data source attribution file name] --mappingFile [mapping tab file name]";
+
+my $fh;
+unless ($DatasetAttributionXML) {
+  print STDERR "outputFile is required/n$usage";
+}
+else {
+#  print STDERR $DatasetAttributionXML;
+  open $fh, '>', $DatasetAttributionXML or die "open ( $DatasetAttributionXML): $!/n$usage";
+}
+
+my %mapping;
+unless ($mappingFile) {
+  print STDERR "mappingFile is required/n$usage";
+}
+
+else {
+  open (FILE, $mappingFile);
+  while (<FILE>) {
+    chomp;
+    my($oldName,$newName) = split("\t");
+    $mapping{$newName} = $oldName unless $newName =~m/^#/;
+  }
+  close (FILE);
+}
+
+while (my ($newName, $oldName) = each %mapping ) {
+  my $legacyResource =  $source -> { 'resource' } -> {$oldName};
+  my $resource =  $newName;
+  my $publication = $legacyResource -> { 'publication' }[0]->{pmid};
+  my $contact_name = $legacyResource -> { 'manualGet' }[0] -> { 'contact' };
+  my $contact_email = $legacyResource -> { 'manualGet' }[0] -> { 'email' };
+  my $contact_institution = $legacyResource -> { 'manualGet' }[0] -> { 'institution' };
+  my $displayName = $legacyResource -> {'displayName'};
+  my $descriptionText = $legacyResource -> { 'description' }[0];
+  my $description = "<![CDATA[ $descriptionText ]]>";
+  my $xmlNode ="
+  <dataSourceAttribution resource=\"$resource\" overridingType=\"\" overridingSubtype=\"\" ignore=\"\">
+       <displayCategory></displayCategory>
+       <publications>
+         <publication  pmid=\"$publication\" />
+       </publications>
+
+       <contacts>
+          <contact isPrimaryContact=\"true\">
+             <name>\"$contact_name\"</name>
+             <email>\"$contact_email\"</email>
+             <institution>\"$contact_institution\"</institution>
+             <address></address>
+             <city></city>
+             <state></state>
+             <country></country>
+             <zip></zip>
+          </contact>
+       </contacts>
+
+       <displayName><![CDATA[$displayName]]></displayName>
+
+       <summary>$description</summary>
+
+       <protocol></protocol>
+       <caveat></caveat>
+       <acknowledgement></acknowledgement>
+       <releasePolicy></releasePolicy>
+
+       <description>$description</description>
+
+       <links>
+          <link>
+             <type>publicUrl</type>
+             <url></url>
+             <linkDescription></linkDescription>
+          </link>
+       </links>
+
+       <wdkReference recordClass=\"\" type=\"\" name=\"\" />
+    </dataSourceAttribution>
+ ";
+
+print $fh $xmlNode;
+
+}
