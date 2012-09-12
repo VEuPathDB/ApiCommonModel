ade6385b845902520b57eb676045ec12af275ac3
Steve Fischer <sfischer@upenn.edu>
2012-09-12 19:49:21 +0000

    new
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@50026 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/templateInjector/TemplateInjector.java
@@ -0,0 +1,9 @@
+package org.apidb.apicommon.model.templateInjector;
+
+import java.util.Properties;
+
+public class TemplateInjector {
+    Properties getPropValues() { return null; }
+    void injectWdkTemplate(String templateName, Properties propValues) {};
+    void injectGbrowseTemplate(String templateName, Properties propValues) {};
+}
