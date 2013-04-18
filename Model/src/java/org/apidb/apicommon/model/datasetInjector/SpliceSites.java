ad33a3ecfbab33432be7107f98f9d63a484aaaa7
Bindu Gajria <bindu.gajria@gmail.com>
2013-04-18 22:04:28 +0000

    add dataset injector for splice sites
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@54957 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/SpliceSites.java
@@ -0,0 +1,74 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class SpliceSites extends  DatasetInjector {
+  
+  /*
+   * getPropValues() gets the property values provided by the datasetPresenter
+   * xml file. they are validated against the names provided in
+   * getPropertyNames(). in the code below, the whole bundle is passed to each
+   * of the templates even though a given template might not need all of them.
+   * this is just convenience, rather than tailoring the list per template, it
+   * is safe to pass all in, because unneeded ones will be ignored.
+   */
+
+  public void injectTemplates() {
+      // TODO:  graphs for the gene record page?
+
+      String projectName = getPropValue("projectName");
+
+      String datasetName = getDatasetName();
+      String[] datasetWords = datasetName.split("_");
+      setPropValue("organismAbbrev", datasetWords[0]);
+
+
+      if(getPropValueAsBoolean("isEuPathDBSite")) {
+          setPropValue("includeProjects", projectName + ",EuPathDB");
+
+      } else {
+          setPropValue("includeProjects", projectName);
+      }
+
+      if(getPropValueAsBoolean("hasMultipleSamples")) {
+	  injectTemplate("spliceSitesProfileSetParamQuery");
+	  injectTemplate("spliceSitesFoldChangeQuestion");
+
+	  injectTemplate("spliceSitesProfileSetsQuery");
+	  injectTemplate("spliceSitesDifferentialQuestion");
+      }
+
+      injectTemplate("spliceSitesPctProfileSetParamQuery");
+      injectTemplate("spliceSitesPercentileQuestion");
+     
+  }
+
+  public void addModelReferences() {
+
+      // TODO: Add reference for Graph
+      if(getPropValueAsBoolean("hasMultipleSamples")) {
+	  addWdkReference("GeneRecordClasses.GeneRecordClass", "question",
+			  "GenesBySpliceSites" + getDatasetName() );
+
+
+	  addWdkReference("GeneRecordClasses.GeneRecordClass", "question",
+			  "GenesByDifferentialSpliceSites" + getDatasetName());
+      }
+
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "question",
+		      "GenesBySpliceSites" + getDatasetName() + "Percentile");
+
+  }
+
+  
+  // declare properties required beyond those inherited from the datasetPresenter
+  // second column is for documentation
+  public String[][] getPropertiesDeclaration() {
+      String [][] declaration = {
+                                 {"isEuPathDBSite", ""},
+                                 {"hasMultipleSamples", "if experiment has just one sample, then NO fold-change or differential Q"}
+      };
+
+    return declaration;
+  }
+}
