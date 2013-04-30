d77fe58d429ed938bd992d5612da0c46b532c221
Ja’Shon Cade <jcade@upenn.edu>
2013-04-30 21:19:19 +0000

    added dataset and java class for gilchrist stages microarray
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@55381 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/AmoebaDB/MicroarrayGilchristStages.java
@@ -0,0 +1,15 @@
+package org.apidb.apicommon.model.datasetInjector.custom.AmoebaDB;
+
+import org.apidb.apicommon.model.datasetInjector.CusomGenePageExpressionGraphs;
+
+public class MicroarrayGilchristStages extends CusomGenePageExpressionGraphs {
+
+  public void addModelReferences() {
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByExpressionTimingPercentileGilchrist"); 
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByEHistolyticaExpressionTiming"); 
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "profile_graph", "Singh::SinghEhTimeSeries"); 
+  }
+
+
+}
+
