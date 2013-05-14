8c8cdabb15c2a6dead750e13f81b588203d73d03
Bindu Gajria <bindu.gajria@gmail.com>
2013-05-14 18:56:22 +0000

    script to fix the BLAST files
    If BLAST webservice files are copied straight from apiSiteFileStaging into apiSiteFiles, then the individual BLAST file names are NOT unique... and this seems to be a requirement of WU BLast.
    This script can be employed after the copy has been made; it prepends each filename with the organismName 
    
    -refs #13200
    
    
    
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@55772 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/bin/fixBlastFiles.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl
+
+use strict;
+
+use XML::Simple;
+use File::Basename;
+use DBI;
+use DBD::Oracle;
+use Getopt::Long;
+
+#use Data::Dumper;
+
+my ($help, $apiSiteFilesDir, $eupathDatabase);
+
+&GetOptions('help|h' => \$help,
+            'apiSiteFilesDir=s' => \$apiSiteFilesDir,
+            'eupath_database=s' => \$eupathDatabase,
+            );
+
+unless(-d $apiSiteFilesDir) {
+  &usage("ERROR:  apiSiteFiles Directory does not exist");
+}
+
+unless(lc($eupathDatabase) =~ /eupa[a|b][n|s]/) {
+  &usage("ERROR:  EuPathDB Database must be specified as one of eupa[a|b][n|s]");
+}
+if($help) {
+  &usage();
+}
+
+my $failures = 0;
+my $dbiDsn = 'dbi:Oracle:' . $eupathDatabase;
+
+my $dbh = DBI->connect($dbiDsn) or die DBI->errstr;
+$dbh->{RaiseError} = 1;
+$dbh->{AutoCommit} = 0;
+
+# model Files we will read
+my $sharedParamsXml = $ENV{PROJECT_HOME} . "/ApiCommonShared/Model/lib/wdk/apiCommonModel/questions/params/sharedParams.xml";
+my $apiCommonModelXml = $ENV{PROJECT_HOME} . "/ApiCommonShared/Model/lib/wdk/apiCommonModel.xml";
+
+my $sharedParams = XMLin($sharedParamsXml, ForceArray => 1);
+my $apiCommonModel = XMLin($apiCommonModelXml, keyattr=>[], ForceArray => 1);
+
+my $blastDatabaseTypes = $sharedParams->{paramSet}->{sharedParams}->{enumParam}->{BlastDatabaseType}->{enumList}->[0]->{enumValue};
+my $blastSql =  $sharedParams->{querySet}->{SharedVQ}->{sqlQuery}->{BlastOrganismFiles}->{sql}->[0];
+$blastSql =~ s/\\'/'/;
+
+my %projectVersions;
+
+foreach(@{$apiCommonModel->{constant}}) {
+  if ($_->{name} eq "buildNumber") {
+    my $version = $_->{content};
+
+    #  more than one project may be assigned the same buildNumber
+    my @projects = split(',', $_->{includeProjects});
+    foreach my $p (@projects) {
+	$projectVersions{$p} = $version;
+    }
+  }
+}
+
+# Get valid project ids
+my $sh = $dbh->prepare("select distinct project_id from ApidbTuning.SequenceAttributes");
+$sh->execute();
+
+while(my ($projectId) = $sh->fetchrow_array()) {
+  next if($projectId eq 'OrphanDB');
+
+  my $version = $projectVersions{$projectId};
+  die "No version for project $projectId specified "unless($version);
+
+  foreach my $blastType (@$blastDatabaseTypes) {
+
+    my $tempBlastSql = $blastSql;
+    my $includeProjects = $blastType->{includeProjects};
+    my $excludeProjects = $blastType->{excludeProjects};
+
+    next if($includeProjects && $includeProjects !~ /$projectId/);
+    next if($excludeProjects && $excludeProjects =~ /$projectId/);
+
+    my $internal = $blastType->{internal}->[0];
+
+    $tempBlastSql =~ s/\@PROJECT_ID\@/$projectId/g;
+    $tempBlastSql =~ s/\$\$BlastDatabaseType\$\$/$internal/g;
+
+    my $blastSh = $dbh->prepare($tempBlastSql);
+    $blastSh->execute();
+
+    while(my ($parent, $organism, $file) = $blastSh->fetchrow_array()) {
+      next if($file eq '-1');
+
+
+      # example $file (in apiSiteFilesStaging) maybe: :
+      #  @WEBSERVICEMIRROR@/ToxoDB/build-%%buildNumber%%/Eimeriidae/blast/
+
+      my $basename = basename($file);     # name of file ("blast" for eg)
+      my $dirname = dirname ($file);
+      my $outerDir = basename($dirname);  # dir of the file  ("Eimeriidae" for eg)
+
+      # make array of $extension
+      my @extensionArr = ('.xnd', '.xns', '.xnt');
+      if($internal eq 'AnnotatedProteins' || $internal eq 'ORFs_AA') {
+	@extensionArr = ('.xpd', '.xps', '.xpt');
+      }
+
+      # prepend file name with organismName ($outerDir)
+      foreach my $extension (@extensionArr){
+	my $filename = "$apiSiteFilesDir/webServices/$projectId/build-$version/$outerDir/$basename/" . $internal . $extension;
+	my $filenameNew = "$apiSiteFilesDir/webServices/$projectId/build-$version/$outerDir/$basename/$outerDir" . $internal . $extension;
+
+	if (-e $filename) {
+	  print  "mv $filename  $filenameNew\n" ;
+	} else {
+	  print "ERROR:  Expected file not found:  $filename\n";
+	  $failures++;
+	}
+      }
+
+    }
+    $blastSh->finish();
+  }
+}
+$sh->finish();
+$dbh->disconnect();
+
+print STDERR "Finished with $failures Errors\n";
+
+sub usage {
+  my $e = shift;
+
+  if($e) {
+    print STDERR $e . "\n";
+  }
+  print STDERR "usage:  fixBlastFiles.pl --apiSiteFilesDir <DIR> --eupath_database eupa[a|b][n|s]\n";
+  exit;
+}
+
+
+1;
+
+
+
+
+
+
+
