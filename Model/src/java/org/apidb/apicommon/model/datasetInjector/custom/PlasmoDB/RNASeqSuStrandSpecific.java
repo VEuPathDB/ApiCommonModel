69906dc29cc2a1bf791daeab1f8098df0c99a658
Haiming Wang <hwang@uga.edu>
2013-07-23 19:31:18 +0000

    comments out GenesByRNASeqSuStrandSpecificPValue question
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@57078 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/RNASeqSuStrandSpecific.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/RNASeqSuStrandSpecific.java
@@ -8,7 +8,7 @@ public class RNASeqSuStrandSpecific extends CusomGenePageExpressionGraphs {
   public void addModelReferences() {
       addWdkReference("GeneRecordClasses.GeneRecordClass", "profile_graph", "Su::PfStrandSpecific"); 
       addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqSuStrandSpecific"); 
-      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqSuStrandSpecificPValue"); 
+      //addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqSuStrandSpecificPValue"); 
       addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqSuStrandSpecificPercentile"); 
       addWdkReference("SequenceRecordClasses.SequenceRecordClass", "gbrowse_track", "pfal3D7_Su_strand_specific_rnaSeq_RSRC_RNASeqCoverage"); 
   }
