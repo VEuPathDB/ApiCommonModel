76af484501035d8d199b4f337650e5369dcb805c
Haiming Wang <hwang@uga.edu>
2013-07-25 21:39:17 +0000

    comments out GeneQuestions.GenesBySuCqPage wdk reference
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@57146 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/MicroarraySuCq.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/MicroarraySuCq.java
@@ -8,9 +8,10 @@ public class MicroarraySuCq extends CusomGenePageExpressionGraphs {
   @Override
   public void addModelReferences() {
       addWdkReference("GeneRecordClasses.GeneRecordClass", "profile_graph", "Su::CQTreatment"); 
-      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesBySuCqPage"); 
       addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesBySuCqFoldChange"); 
       addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesBySuCqPercentile"); 
+
+      //addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesBySuCqPage"); 
   }
 
 
