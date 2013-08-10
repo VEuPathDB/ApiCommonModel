35338c375f137b5e7428511385de2485213c6fd5
John Brestelli <jbrestell@gmail.com>
2013-08-10 14:47:54 +0000

    add microarraytwochanneldirect
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@57440 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/MicroarrayTwoChannelDirectComparison.java
@@ -0,0 +1,33 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+
+public class MicroarrayTwoChannelDirectComparison extends ExpressionTwoChannelDirectComparison {
+
+
+
+    @Override
+    protected void setExprPlotPartModule() {
+        setPropValue("exprPlotPartModule", "LogRatio");
+    }
+
+
+    @Override
+    protected void setExprGraphVisiblePart() {
+        setPropValue("exprGraphVisiblePart", "exprn_val");
+    }
+
+    @Override
+    protected void setGraphModule() {
+        setPropValue("graphModule", "Microarray::TwoChannel");
+    }
+
+
+    @Override
+    protected void setGraphYAxisDescription() {
+        String yAxisDescription = "Expression Values for 2 channel microarray experiments are log ratios (M = log2 Cy5/Cy3).  We also provide the fold difference in the right axis.  For any 2 points on the graph (M1, M2) the  fold difference is calculated by:  power(2, (M2-M1)).   or expression percentile value.";
+
+        setPropValue("graphYAxisDescription", yAxisDescription);
+    }
+
+
+}
