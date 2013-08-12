e1f22eb74a51f8222e701140b2806b5449068c5a
Haiming Wang <hwang@uga.edu>
2013-08-12 19:52:45 +0000

    minor change
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@57468 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
@@ -14,12 +14,11 @@ public class IsolatesHTS extends DatasetInjector {
       setOrganismAbbrevInternalFromDatasetName();
 
       // trim off the prefix and suffix from the experiment name
-      String experimentRsrc = datasetName.replaceFirst(datasetWords[0] + "_SNP_", "");
-
       // new workflow uses different naming convention for HTS isolates, need to handle both.
       // pfal3D7_SNP_Conway_HTS_SNP_RSRC (build-18)
       // pfal3D7_HTS_SNP_Conway_HTS_SNP_RSRC (build-19)
       String experimentRsrc = datasetName.replaceFirst(datasetWords[0] + "_HTS_SNP_", "");
+      experimentRsrc = datasetName.replaceFirst(datasetWords[0] + "_SNP_", "");
 
       String experimentName = experimentRsrc.replaceFirst("_RSRC", "");
       setPropValue("experimentName", experimentName);
