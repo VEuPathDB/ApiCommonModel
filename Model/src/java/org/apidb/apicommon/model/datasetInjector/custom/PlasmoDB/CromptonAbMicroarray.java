81e9621ab6b1fbb46a06ec9c9c4646e195cc7746
Ja’Shon Cade <jcade@upenn.edu>
2013-08-16 21:00:39 +0000

    custom injector for Crompton dataset
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@57577 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/CromptonAbMicroarray.java
@@ -0,0 +1,25 @@
+package org.apidb.apicommon.model.datasetInjector.custom.PlasmoDB;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class CromptonAbMicroarray extends DatasetInjector {
+
+  @Override
+  public void injectTemplates() {
+  }
+
+  @Override
+  public void addModelReferences() {
+    addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesbyCromptonAbFoldChange");
+  }
+
+  // second column is for documentation
+  @Override
+  public String[][] getPropertiesDeclaration() {
+    String[][] propertiesDeclaration = {};
+    return propertiesDeclaration;
+  }
+
+
+
+}
