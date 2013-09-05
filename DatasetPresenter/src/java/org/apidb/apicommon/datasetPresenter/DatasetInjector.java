468c1d62a8ed2549618272e767d3abd1e5aa5009
Xin Gao <xingao@upenn.edu>
2013-09-05 17:25:14 +0000

    add a DiscoverableDatasetInjector that can discover injectors by the given properties.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@58052 735e2a04-f8fc-0310-8a1b-f2942603c481

--- DatasetPresenter/src/java/org/apidb/apicommon/datasetPresenter/DatasetInjector.java
+++ DatasetPresenter/src/java/org/apidb/apicommon/datasetPresenter/DatasetInjector.java
@@ -23,7 +23,7 @@ public abstract class DatasetInjector {
   private String datasetName;
   private Contact primaryContact;
   private DatasetInjectorSet datasetInjectorSet;
-  private Map<String, ModelReference> modelReferences = new HashMap<String,ModelReference>();
+  private Map<String, ModelReference> modelReferences = new HashMap<String, ModelReference>();
 
   /**
    * Subclasses use this method to declare the properties they require. These
@@ -45,26 +45,25 @@ public abstract class DatasetInjector {
    */
   protected abstract String[][] getPropertiesDeclaration();
 
+  protected String[][] combinePropertiesDeclarations(String[][] one,
+      String[][] two) {
+    String[][] combined = new String[one.length + two.length][2];
 
-  protected String[][] combinePropertiesDeclarations(String[][] one, String[][] two) {
-      String[][] combined = new String[one.length + two.length][2];
+    for (int i = 0; i < one.length; i++) {
+      combined[i] = one[i];
+    }
+    for (int i = 0; i < two.length; i++) {
+      combined[i + one.length] = two[i];
+    }
 
-      for(int i = 0; i < one.length; i++) {
-          combined[i] = one[i];
-      }
-      for(int i = 0; i < two.length; i++) {
-          combined[i + one.length] = two[i];
-      }
-
-      return combined;
+    return combined;
   }
 
- 
   /**
-   * Subclasses call this method to add model references to the
-   * presentation layer. To do so they add a call in this method to either
-   * {@link #addWdkReference(String,String,String} or {@link #addModelReference(String, String). 
-   * 
+   * Subclasses call this method to add model references to the presentation
+   * layer. To do so they add a call in this method to either {@link
+   * #addWdkReference(String,String,String} or
+   * {@link #addModelReference(String, String). *
    */
   protected abstract void addModelReferences();
 
@@ -74,8 +73,8 @@ public abstract class DatasetInjector {
    * {@link #injectTemplate(String)}. The template will have available all
    * properties declared by <code>getPropertiesDeclaration</code>.
    * 
-   * Optionally they provide additional hard-coded properties to templates by calling
-   * {@link #setPropValue(String, String)} before calling
+   * Optionally they provide additional hard-coded properties to templates by
+   * calling {@link #setPropValue(String, String)} before calling
    * <code>injectTemplate()</code>.
    */
   protected abstract void injectTemplates();
@@ -93,20 +92,20 @@ public abstract class DatasetInjector {
   }
 
   protected String getPropValue(String key) {
-      return propValues.get(key);
+    return propValues.get(key);
   }
 
   protected boolean getPropValueAsBoolean(String key) {
-      return Boolean.parseBoolean(propValues.get(key));
+    return Boolean.parseBoolean(propValues.get(key));
   }
 
   protected String getDatasetName() {
     return datasetName;
   }
 
-    protected Contact getPrimaryContact() {
-        return this.primaryContact;
-    }
+  protected Contact getPrimaryContact() {
+    return this.primaryContact;
+  }
 
   /**
    * Subclasses should call this method inside {@link #injectTemplates()} to
@@ -129,14 +128,16 @@ public abstract class DatasetInjector {
    * @param name
    */
   protected void addWdkReference(String recordClass, String type, String name) {
-    ModelReference ref = new ModelReference(recordClass, type, name, datasetName);
+    ModelReference ref = new ModelReference(recordClass, type, name,
+        datasetName);
     String key = recordClass + type + name;
     if (modelReferences.containsKey(key)) {
-      throw new UserException("Dataset " + datasetName + " already contains a model reference for " + type + ", " + name);
+      throw new UserException("Dataset " + datasetName
+          + " already contains a model reference for " + type + ", " + name);
     }
     modelReferences.put(key, ref);
   }
-  
+
   /**
    * Subclasses should call this method inside {@link #addModelReferences()} to
    * make a reference to a model object that is not in the WDK (eg, GBrowse).
@@ -149,21 +150,22 @@ public abstract class DatasetInjector {
     ModelReference ref = new ModelReference(type, name, datasetName);
     String key = type + name;
     if (modelReferences.containsKey(key)) {
-      throw new UserException("Dataset " + datasetName + " already contains a model reference for " + type + ", " + name);
+      throw new UserException("Dataset " + datasetName
+          + " already contains a model reference for " + type + ", " + name);
     }
     modelReferences.put(key, ref);
   }
-  
+
   List<ModelReference> getModelReferences() {
     return new ArrayList<ModelReference>(modelReferences.values());
   }
 
-
   /**
    * Add property values.
+   * 
    * @param propValues
    */
-  void addPropValues(Map<String, String> propValues) {
+  protected void addPropValues(Map<String, String> propValues) {
     this.propValues.putAll(propValues);
 
     // validate against declaration
@@ -178,97 +180,113 @@ public abstract class DatasetInjector {
   }
 
   /**
+   * This method is used to discover if a datasetInjector is a potential match
+   * given the current properties. The default behavior is that if all the
+   * declared properties are present, then the injector is considered a match.
+   * Subclasses can override this method to change the discover behavior or add
+   * additional checks.
+   * 
+   * @param propValues
+   * @return    true if the injector is a match with given param values.
+   */
+  protected boolean discover(Map<String, String> propValues) {
+    String[][] propsDeclaration = getPropertiesDeclaration();
+    boolean hasProp = true;
+    for (String[] decl : propsDeclaration) {
+      if (!propValues.containsKey(decl[0])) {
+        hasProp = false;
+        break;
+      }
+    }
+    // if all the declared properties are present, consider this dataset
+    // injector a potential match; otherwise, not match.
+    return hasProp;
+  }
+
+  /**
    * Set the name of the dataset that is being injected.
+   * 
    * @param datasetName
    */
   void setDatasetName(String datasetName) {
     this.datasetName = datasetName;
   }
 
-
   /**
    * Set the name of the dataset that is being injected.
+   * 
    * @param primaryContact
    */
   void setPrimaryContact(Contact primaryContact) {
-      this.primaryContact = primaryContact;
+    this.primaryContact = primaryContact;
   }
-
-
 
   /**
    * Set the parent DatasetInjectorSet.
+   * 
    * @param datasetInjectorSet
    */
   void setDatasetInjectorSet(DatasetInjectorSet datasetInjectorSet) {
     this.datasetInjectorSet = datasetInjectorSet;
   }
 
-
-    /***
-     *   return the first word before the underscore in a dataset name
-     *    for organism specific .. this is always the orgAbbrev
-     *   
-     */
-    protected String getOrganismAbbrevFromDatasetName() {
-        if(this.datasetName.equals("")) {
-            return "";
-        }
-
-        // These conditions are a band-aid until we can read the organism xml file in EUPathDatasets
-        if(this.datasetName.substring(0, 4).equals("gass")) {
-            return "G.l.";
-        }
-
-        if(this.datasetName.substring(0, 5).equals("Phyra")) {
-            return "P.r.";
-        }
-
-        if(this.datasetName.substring(0, 5).equals("Hyaar")) {
-            return "H.a.";
-        }
-
-
-
-
-
-        return this.datasetName.substring(0, 1).toUpperCase() + "." + this.datasetName.substring(1, 2).toLowerCase() + ".";
+  /***
+   * return the first word before the underscore in a dataset name for organism
+   * specific .. this is always the orgAbbrev
+   * 
+   */
+  protected String getOrganismAbbrevFromDatasetName() {
+    if (this.datasetName.equals("")) {
+      return "";
     }
 
-
-    protected void setOrganismAbbrevFromDatasetName() {
-        String organismAbbrev = this.getOrganismAbbrevFromDatasetName();
-        setPropValue("organismAbbrev", organismAbbrev);
+    // These conditions are a band-aid until we can read the organism xml file
+    // in EUPathDatasets
+    if (this.datasetName.substring(0, 4).equals("gass")) {
+      return "G.l.";
     }
 
-
-    protected void setOrganismAbbrevInternalFromDatasetName() {
-        String[] datasetWords = this.datasetName.split("_");
-        setPropValue("organismAbbrevInternal", datasetWords[0]);
+    if (this.datasetName.substring(0, 5).equals("Phyra")) {
+      return "P.r.";
     }
 
-
-    protected void setGraphDatasetName() {
-        String graphDatasetName = this.datasetName.replace("-", "");
-        setPropValue("graphDatasetName", graphDatasetName);
+    if (this.datasetName.substring(0, 5).equals("Hyaar")) {
+      return "H.a.";
     }
-    
 
+    return this.datasetName.substring(0, 1).toUpperCase() + "."
+        + this.datasetName.substring(1, 2).toLowerCase() + ".";
+  }
 
-    protected void setShortAttribution() {
-      String shortAttribution = getPropValue("shortAttribution");
+  protected void setOrganismAbbrevFromDatasetName() {
+    String organismAbbrev = this.getOrganismAbbrevFromDatasetName();
+    setPropValue("organismAbbrev", organismAbbrev);
+  }
 
-      if(shortAttribution == null || shortAttribution.equals("")) {
-          Contact primaryContact = getPrimaryContact();
+  protected void setOrganismAbbrevInternalFromDatasetName() {
+    String[] datasetWords = this.datasetName.split("_");
+    setPropValue("organismAbbrevInternal", datasetWords[0]);
+  }
 
-          String contactName = primaryContact.getName();
-          int lastSpace = contactName.lastIndexOf(" ");
+  protected void setGraphDatasetName() {
+    String graphDatasetName = this.datasetName.replace("-", "");
+    setPropValue("graphDatasetName", graphDatasetName);
+  }
 
-          setPropValue("shortAttribution", "(" + contactName.substring(lastSpace + 1) + ")");
-      } 
-      else {
-          setPropValue("shortAttribution", "(" + shortAttribution + ")");
-      }
+  protected void setShortAttribution() {
+    String shortAttribution = getPropValue("shortAttribution");
+
+    if (shortAttribution == null || shortAttribution.equals("")) {
+      Contact primaryContact = getPrimaryContact();
+
+      String contactName = primaryContact.getName();
+      int lastSpace = contactName.lastIndexOf(" ");
+
+      setPropValue("shortAttribution",
+          "(" + contactName.substring(lastSpace + 1) + ")");
+    } else {
+      setPropValue("shortAttribution", "(" + shortAttribution + ")");
     }
+  }
 
 }
---
+++ Test/src/java/org/apidb/apicommon/datasetPresenter/DiscoverableDatasetInjectorTest.java
@@ -0,0 +1,57 @@
+package org.apidb.apicommon.datasetPresenter;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.apidb.apicommon.model.datasetInjector.custom.AmoebaDB.MicroarrayGilchristAmoebapore;
+import org.apidb.apicommon.model.datasetInjector.custom.PlasmoDB.ChIPChip;
+import org.apidb.apicommon.model.datasetInjector.custom.PlasmoDB.PathwayGinsburg;
+import org.gusdb.fgputil.xml.Text;
+
+public class DiscoverableDatasetInjectorTest {
+
+  public static void main(String[] args) {
+    // pick test injectors
+    DatasetInjector[] injectors = { new MicroarrayGilchristAmoebapore(),
+        new ChIPChip(), new PathwayGinsburg() };
+
+    // construct property values
+    Map<String, String> propValues = new HashMap<>();
+    Random random = new Random();
+    for (DatasetInjector injector : injectors) {
+      for (String[] decl : injector.getPropertiesDeclaration()) {
+        propValues.put(decl[0], "value-" + random.nextInt());
+      }
+    }
+
+    // construct DiscoverableDatasetInjector
+    Contact contact = new Contact();
+    contact.setName(new Text("Contact-" + random.nextInt()));
+    DiscoverableDatasetInjector discoverer = new DiscoverableDatasetInjector();
+    discoverer.setDatasetName("Dataset-" + random.nextInt());
+    discoverer.setPrimaryContact(contact);
+    discoverer.addPropValues(propValues);
+
+    Collection<DatasetInjector> discovered = discoverer.getInjectors();
+    for (DatasetInjector injector : injectors) {
+      String name = injector.getClass().getName();
+      boolean hasName = false;
+      for (DatasetInjector disc : discovered) {
+        String discName = disc.getClass().getName();
+        if (discName.equals(name)) {
+          hasName = true;
+          break;
+        }
+      }
+      if (!hasName)
+        System.err.println("The DatasetInjector '" + name
+            + "' failed to be discovered");
+    }
+    System.out.println("Totally discovered " + discovered.size() + " injectors.");
+    for (DatasetInjector injector : discovered) {
+      System.out.println(injector.getClass().getName());
+    }
+  }
+}
