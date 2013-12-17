eb555ec0d4682018e050f86105663771d7f08ab1
Haiming Wang <hwang@uga.edu>
2013-12-17 21:08:53 +0000

    "isEuPathDBSite" and "optionalSpecies" props are required for annotated genome injector
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@59578 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/AnnotatedGenome.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/AnnotatedGenome.java
@@ -89,7 +89,9 @@ public class AnnotatedGenome extends DatasetInjector {
   // second column is for documentation
   @Override
   public String[][] getPropertiesDeclaration() {
-    String [][] propertiesDeclaration = { };
+    String [][] propertiesDeclaration = { { "isEuPathDBSite", "if true, genome will be available on EuPathdB" },
+                                          { "optionalSpecies", "if species name contains two words, e.g. sp. 1" },
+                                        };
 
     return propertiesDeclaration;
   } 
