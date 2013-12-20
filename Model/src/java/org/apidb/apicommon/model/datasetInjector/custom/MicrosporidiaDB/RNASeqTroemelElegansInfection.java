8b935b37662f9cff2a15179d480d2abf63614e53
Ryan Doherty <tech@conical.org>
2013-12-20 19:27:24 +0000

    fix whitespace and uncomment unused vars
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@59658 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/MicrosporidiaDB/RNASeqTroemelElegansInfection.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/MicrosporidiaDB/RNASeqTroemelElegansInfection.java
@@ -4,25 +4,23 @@ import org.apidb.apicommon.model.datasetInjector.CusomGenePageExpressionGraphs;
 
 public class RNASeqTroemelElegansInfection extends CusomGenePageExpressionGraphs {
 
-   @Override
-   public void injectTemplates() {
- 
-       String projectName = getPropValue("projectName");
- 
-       String datasetName = getDatasetName();
- 
-       setOrganismAbbrevFromDatasetName();
-       setOrganismAbbrevInternalFromDatasetName();
- 
-       injectTemplate("rnaSeqCoverageTrack");
-       injectTemplate("rnaSeqJunctionsTrack");
-   } 
+  @Override
+  public void injectTemplates() {
+
+    //String projectName = getPropValue("projectName");
+    //String datasetName = getDatasetName();
+
+    setOrganismAbbrevFromDatasetName();
+    setOrganismAbbrevInternalFromDatasetName();
+
+    injectTemplate("rnaSeqCoverageTrack");
+    injectTemplate("rnaSeqJunctionsTrack");
+  }
 
   @Override
   public void addModelReferences() {
-      addWdkReference("GeneRecordClasses.GeneRecordClass", "profile_graph", "Troemel::CelegansInfectionTimeSeries"); 
-      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqTroemelCeInfectionFC"); 
-      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqTroemelCeInfectionPercentile"); 
+    addWdkReference("GeneRecordClasses.GeneRecordClass", "profile_graph", "Troemel::CelegansInfectionTimeSeries");
+    addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqTroemelCeInfectionFC");
+    addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByRNASeqTroemelCeInfectionPercentile");
   }
-  
 }
