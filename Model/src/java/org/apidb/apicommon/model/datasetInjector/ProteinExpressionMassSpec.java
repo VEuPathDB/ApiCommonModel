4d25e37463d85594a5c063961b8ce065b53353f2
John Brestelli <jbrestell@gmail.com>
2014-01-08 17:23:03 +0000

    remove orfs by mass spec question (fixes #14925)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@59752 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/ProteinExpressionMassSpec.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/ProteinExpressionMassSpec.java
@@ -36,7 +36,6 @@ public class ProteinExpressionMassSpec extends DatasetInjector {
   @Override
   public void addModelReferences() {
       addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByMassSpec");
-      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "OrfQuestions.OrfsByMassSpec");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "table", "MassSpecMod");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "table", "MassSpecDownload");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "table", "MassSpec");
