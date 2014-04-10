28cf3c3a85f565007d4d5bf471781b7a7e2510b9
Ja’Shon Cade <jcade@upenn.edu>
2014-04-10 18:01:45 +0000

    new injector for antibodyArray
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@61518 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/AntibodyArray.java
@@ -0,0 +1,113 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class AntibodyArray extends DatasetInjector {
+
+    protected void setGraphModule() {
+        setPropValue("graphModule", "Crompton::AbMicroarray");
+    }
+
+    protected void setGraphYAxisDescription() {
+        String yAxisDescription = "Arcsinh(1+50x) transform of signal intensity";
+
+        setPropValue("graphYAxisDescription", yAxisDescription);
+    }
+
+    protected void setProfileSetFilterPattern() {
+        setPropValue("profileSetFilterPattern", "%");
+    }
+
+
+
+    @Override
+    public void addModelReferences() {
+      setGraphModule();
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "profile_graph", getPropValue("graphModule") + getDatasetName() ); 
+    }
+
+    @Override
+    public void injectTemplates() {
+
+        // perl packages disallow some characters in the package name... use this to name the graphs
+        setGraphDatasetName();
+
+
+        setPropValue("organismAbbrev", getOrganismAbbrevFromDatasetName());
+
+        String projectName = getPropValue("projectName");
+
+        if(getPropValueAsBoolean("isEuPathDBSite")) {
+            setPropValue("includeProjects", projectName + ",EuPathDB");
+            
+        } else {
+            setPropValue("includeProjects", projectName);
+        }
+
+
+        setGraphModule();
+        setGraphYAxisDescription();
+        injectTemplate("antibodyArrayProfileSetParamQuery");
+
+        injectTemplate("antibodyArrayQuestion");
+
+        injectTemplate("antibodyArrayFoldChangeCategories");
+
+        injectTemplate("antibodyArrayAttributeCategory");
+
+        injectTemplate("antibodyArrayGraphAttributesExpression");
+
+
+
+        injectTemplate("pathwayGraphs");
+
+            setPropValue("graphVisibleParts", "xy_scatter");
+    
+
+        // these are universal for injected expression experiments
+        setPropValue("graphGenePageSection", "host response");
+
+        if(getPropValue("graphPriorityOrderGrouping").equals("")) {
+            setPropValue("graphPriorityOrderGrouping", "1");
+        }
+        
+        // need to make sure there are no single quotes in the descrip
+        String datasetDescrip = getPropValue("datasetDescrip");
+        setPropValue("datasetDescrip", datasetDescrip.replace("'", ""));
+
+        setPropValue("isGraphCustom", "true");
+
+        injectTemplate("antibodyArrayGraphDescriptions");
+
+        injectTemplate("datasetExampleGraphDescriptions");
+
+        String excludeProfileSets = getPropValue("excludedProfileSets");
+
+        String excludedProfileSetsList = "'INTERNAL'";
+        if(!excludeProfileSets.equals("")) {
+            String[] excludedProfileSetsArr = excludeProfileSets.split(";");
+
+            for(int i = 0; i < excludedProfileSetsArr.length; i++) {
+                excludedProfileSetsList = excludedProfileSetsList + ", '" + excludedProfileSetsArr[i] + "'";
+            }
+        }
+
+        setPropValue("excludedProfileSetsList", excludedProfileSetsList);
+    }
+
+    @Override
+    public String[][] getPropertiesDeclaration() {
+        String [][] declaration = {
+                                   {"isEuPathDBSite", ""},
+                                   {"excludedProfileSets", "OPTIONAL PROP... extra profile sets are loaded which we need to exclude from the graphs and questions"},
+                                   {"optionalQuestionDescription", "This text will be appended to fold change and percentile questions"},
+                                   {"graphXAxisSamplesDescription", "will show up on the gene record page next to the graph"},
+                                   {"graphPriorityOrderGrouping", "numeric grouping / ordering of graphs on the gene record page"},
+                                   {"graphModule", ""},
+        };
+
+        
+        return declaration;
+  }
+}
+
