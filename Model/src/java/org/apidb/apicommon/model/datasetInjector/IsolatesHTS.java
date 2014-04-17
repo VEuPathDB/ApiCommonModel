075993331881c367056dc85d4929b75ef3558b99
Haiming Wang <hwang@uga.edu>
2014-04-17 17:01:47 +0000

    add wdkreference - refs #15498
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@61653 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
@@ -72,6 +72,7 @@ public class IsolatesHTS extends DatasetInjector {
       addWdkReference("IsolateRecordClasses.IsolateRecordClass", "table", "Reference");
       addWdkReference("IsolateRecordClasses.IsolateRecordClass", "table", "HtsContacts");
       addWdkReference("IsolateRecordClasses.IsolateRecordClass", "attribute", "overview");
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "table", "HtsSNPsAlignment");
   }
 
 
