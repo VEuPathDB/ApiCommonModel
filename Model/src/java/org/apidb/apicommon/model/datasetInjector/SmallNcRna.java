398c8fb1c0caa5384a738f63ec727549ca7ac576
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2014-06-20 12:26:04 +0000

    Added DatasetInjector subclass for small non-coding RNAs (refs #16504)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@62885 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/SmallNcRna.java
@@ -0,0 +1,54 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+import java.util.List;
+
+public class SmallNcRna extends  DatasetInjector {
+  
+  /*
+   * getPropValues() gets the property values provided by the datasetPresenter
+   * xml file. they are validated against the names provided in
+   * getPropertyNames(). in the code below, the whole bundle is passed to each
+   * of the templates even though a given template might not need all of them.
+   * this is just convenience, rather than tailoring the list per template, it
+   * is safe to pass all in, because unneeded ones will be ignored.
+   */
+
+  @Override
+  public void injectTemplates() {
+
+//Props from .prop for experiment
+      String projectName = getPropValue("projectName");
+      String organismAbbrev = getPropValue("organismAbbrev");
+      String experimentName = getPropValue("experimentName");
+
+      setPropValue ("organismAbbrevDisplay", getOrganismAbbrevDisplayFromDatasetName().replace(":", " "));
+
+      
+      String sampleNamePrefix = projectName + ":" + organismAbbrev + "_" + experimentName + "_";
+      String sampleNameSuffix = "_smallNcRnaSample_RSRC";
+      List<String> sampleNames = getSampleList(sampleNamePrefix, sampleNameSuffix);
+      
+      for (int i=0; i<sampleNames.size(); i++){
+          setPropValue("sampleName", sampleNames.get(i));
+          injectTemplate("smallNcRnaSampleDatabase");
+          injectTemplate("smallNcRnaSampleTrack");
+      }
+
+  }
+
+
+  @Override
+  public void addModelReferences() {
+  }
+  
+  // declare properties required beyond those inherited from the datasetPresenter
+  // second column is for documentation
+  @Override
+  public String[][] getPropertiesDeclaration() {
+      String [][] declaration = {
+       };
+
+    return declaration;
+  }
+}
