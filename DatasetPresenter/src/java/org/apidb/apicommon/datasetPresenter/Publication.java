3226419fa9acd7a001ed37f01abdec7b0dd54edd
John Brestelli <jbrestell@gmail.com>
2014-06-25 22:42:17 +0000

    merge from trunk
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/branches/gus4@63013 735e2a04-f8fc-0310-8a1b-f2942603c481

--- DatasetPresenter/src/java/org/apidb/apicommon/datasetPresenter/Publication.java
+++ DatasetPresenter/src/java/org/apidb/apicommon/datasetPresenter/Publication.java
@@ -13,19 +13,28 @@ public class Publication {
     }
 
     public String getCitation() {
+        if (pubmedId == null || pubmedId.equals("")) return null;
         if (citation == null) {
             try {
                 byte[] bo = new byte[100000];
+                byte[] err = new byte[100000];
                 String[] cmd = { "pubmedIdToCitation", pubmedId };
                 Process p = Runtime.getRuntime().exec(cmd);
                 p.waitFor();
                 p.getInputStream().read(bo);
+                p.getErrorStream().read(err);
+		if (p.exitValue() != 0) {
+		  throw new UnexpectedException("Failed running 'pubmedIdToCitation "
+						+ pubmedId + "'  --  " + new String(err));
+		}
                 p.destroy();
                 citation = new String(bo).trim();
             } catch (Exception e) {
                 throw new UnexpectedException("Failed running: pubmedIdToCitation "
                                               + pubmedId, e);
             }
+	    if (citation == null) throw new UnexpectedException("Failed running: pubmedIdToCitation "
+								+ pubmedId + " -- returned null citation");
         }
         return citation;
     }
