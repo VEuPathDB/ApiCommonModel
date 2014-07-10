4290f6ea6d88218eca6135e794a7469dad481668
Bindu Gajria <bindu.gajria@gmail.com>
2014-07-10 19:46:47 +0000

    add injector for Toxo and geneByEcNumber Q - refs #16379
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@63272 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/MetabolicPathways.java
@@ -0,0 +1,24 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class MetabolicPathways extends DBXRefs {
+
+  @Override
+  public void injectTemplates() {
+  }
+
+  @Override
+  public void addModelReferences() {
+    addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GeneQuestions.GenesByEcNumber");
+  }
+
+  // second column is for documentation
+  @Override
+  public String[][] getPropertiesDeclaration() {
+    String[][] propertiesDeclaration = {};
+    return propertiesDeclaration;
+  }
+
+  
+}
