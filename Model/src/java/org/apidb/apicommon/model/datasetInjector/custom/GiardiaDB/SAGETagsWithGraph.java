ad60bcfff3be3ebda718ffcc07aec0a0631af977
Haiming Wang <hwang@uga.edu>
2014-08-14 01:40:37 +0000

    refs #16861
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@64049 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/GiardiaDB/SAGETagsWithGraph.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/GiardiaDB/SAGETagsWithGraph.java
@@ -24,6 +24,7 @@ public class SAGETagsWithGraph extends SAGETags {
       injectTemplate("datasetExampleGraphDescriptions");
 
       injectTemplate("sageTagAttributesList");
+      injectTemplate("sageTagAttributesListR");
       injectTemplate("sageTagAttributeCategory");
       injectTemplate("sageTagExpressionGraphAttributes");
 
