41aa6eabe74014aebf4c1bd1a2e7be51dba583f4
Ryan Doherty <tech@conical.org>
2015-08-28 06:20:38 +0000

    remove unused imports
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/branches/gus4@69570 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/ssgcid/Ssgcid.java
+++ Model/src/java/org/apidb/apicommon/model/ssgcid/Ssgcid.java
@@ -1,11 +1,9 @@
 package org.apidb.apicommon.model.ssgcid;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.io.File;
-import java.lang.Runtime;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
