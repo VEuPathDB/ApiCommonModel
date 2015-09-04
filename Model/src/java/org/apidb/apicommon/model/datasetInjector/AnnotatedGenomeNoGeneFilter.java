2be50587e4fc636ce80825f8a54a00201918f968
Ryan Doherty <tech@conical.org>
2015-09-04 11:42:09 +0000

    clean up class
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@69690 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/AnnotatedGenomeNoGeneFilter.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/AnnotatedGenomeNoGeneFilter.java
@@ -1,14 +1,9 @@
 package org.apidb.apicommon.model.datasetInjector;
 
-import org.apidb.apicommon.datasetPresenter.DatasetInjector;
-
 public class AnnotatedGenomeNoGeneFilter extends AnnotatedGenome {
 
     public AnnotatedGenomeNoGeneFilter () {
-
-	super();
-	hasFilters = false;
+      hasFilters = false;
     }
-
 
 }
