1dd24387a7bf9cf5c1f3f4805b45b86609f8ec12
John Brestelli <jbrestell@gmail.com>
2016-01-28 20:42:29 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/branches/gus4@72136 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/ChIPSeq.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/ChIPSeq.java
@@ -10,31 +10,9 @@ public class ChIPSeq extends DatasetInjector {
       setShortAttribution();
       setOrganismAbbrevFromDatasetName();
 
-      String scale = getPropValue("scale");
-
-      if(scale.toLowerCase().equals("log")) {
-          injectTemplate("chipSeqCoverageTrack");
-          setPropValue("gbrowseTrackName", getDatasetName() + "Coverage");
-          injectTemplate("gbrowseTrackCategory");
-      } 
-      else if(scale.toLowerCase().equals("linear")) {
-          injectTemplate("chipSeqCoverageTrackUnlogged");
-          setPropValue("gbrowseTrackName", getDatasetName() + "CoverageUnlogged");
-          injectTemplate("gbrowseTrackCategory");
-      } 
-      else if(scale.toLowerCase().equals("both")) {
-          injectTemplate("chipSeqCoverageTrack");
-          injectTemplate("chipSeqCoverageTrackUnlogged");
-
-          setPropValue("gbrowseTrackName", getDatasetName() + "Coverage");
-          injectTemplate("gbrowseTrackCategory");
-          setPropValue("gbrowseTrackName", getDatasetName() + "CoverageUnlogged");
-          injectTemplate("gbrowseTrackCategory");
-
-      } 
-      else {
-          throw new WdkRuntimeException("property [scale] should be one of [log,linear,both]");
-      }
+      injectTemplate("chipSeqCoverageTrackUnlogged");
+      setPropValue("gbrowseTrackName", getDatasetName() + "CoverageUnlogged");
+      injectTemplate("gbrowseTrackCategory");
 
   }
 
@@ -42,12 +20,10 @@ public class ChIPSeq extends DatasetInjector {
   public void addModelReferences() { }
 
 
-  @Override
-  public String[][] getPropertiesDeclaration() {
-      String [][] declaration = { 
-                      {"scale", "log, linear or both"}, 
-                                };
-      return declaration;
-  }
+    @Override
+	public String[][] getPropertiesDeclaration() {
+        String[][] propertiesDeclaration = {};
+        return propertiesDeclaration;
+    }
 
 }
