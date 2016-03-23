2579f16d5c15b143d2f3d1138b6508589e303359
Bindu Gajria <bindu.gajria@gmail.com>
2016-03-23 19:14:05 +0000

    fix for ProfileSimilarity Q for pfal3D7_microarrayExpression_Derisi_HB3_TimeSeries_RSRC
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/branches/gus4@73470 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/MicroarrayDeRisiTimeSeries.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/MicroarrayDeRisiTimeSeries.java
@@ -1,16 +1,13 @@
 package org.apidb.apicommon.model.datasetInjector.custom.PlasmoDB;
 
-import org.apidb.apicommon.model.datasetInjector.CusomGenePageExpressionGraphs;
+import org.apidb.apicommon.model.datasetInjector.MicroarrayTwoChannelReferenceDesign;
 
-public class MicroarrayDeRisiTimeSeries extends CusomGenePageExpressionGraphs {
+public class MicroarrayDeRisiTimeSeries extends MicroarrayTwoChannelReferenceDesign {
 
   @Override
   public void addModelReferences() {
-      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "profile_graph", "OverlayIRBC::Ver2"); 
-      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "profile_graph", "DeRisi::Combined"); 
-      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByExpressionTiming"); 
+      super.addModelReferences();
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByProfileSimilarity"); 
-      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByMicroarrayPercentileDerisi"); 
   }
 
 
