7577cbc635b2a7566ba92a1f343563f53d929b1a
John Brestelli <jbrestell@gmail.com>
2016-06-02 00:36:49 +0000

    inject reference organisms
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/branches/gus4@74758 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/NcbiTaxonomy.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/NcbiTaxonomy.java
@@ -1,11 +1,52 @@
 package org.apidb.apicommon.model.datasetInjector;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.apidb.apicommon.datasetPresenter.DatasetInjector;
 
 public class NcbiTaxonomy extends DatasetInjector {
 
   @Override
   public void injectTemplates() {
+
+    Map<String, Map<String, String>> globalProps = getGlobalDatasetProperties();
+
+    Map<String, String> refOrgs = new HashMap<String, String>();
+
+    // Find all reference organisms and inject for organism param default
+    for (Map.Entry<String, Map<String, String>> props : globalProps.entrySet()) {
+        Map<String, String> map = props.getValue();
+        if(map.containsKey("isReferenceStrain") && 
+           map.get("isReferenceStrain").toLowerCase().equals("true") &&
+           props.getKey().startsWith(map.get("projectName")) ) {
+            
+            String projectName = map.get("projectName");
+            String organismFullName = map.get("organismFullName");
+
+
+            if(refOrgs.containsKey(projectName)) {
+                String refOrgsString = refOrgs.get(projectName);
+
+                refOrgsString = refOrgsString + "," + organismFullName;
+                refOrgs.put(projectName, refOrgsString);
+                refOrgs.put("EuPathDB", refOrgsString);
+            }
+            else {
+                refOrgs.put(projectName, organismFullName);
+                refOrgs.put("EuPathDB", organismFullName);
+            }
+        }
+    }
+
+    
+    for (Map.Entry<String, String> refOrg : refOrgs.entrySet()) {
+        setPropValue("projectName", refOrg.getKey());
+        setPropValue("referenceOrganisms", refOrg.getValue());
+
+        injectTemplate("referenceOrganisms");
+    }
+
   }
 
   @Override
