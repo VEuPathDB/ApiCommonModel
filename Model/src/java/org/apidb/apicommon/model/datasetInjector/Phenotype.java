7b946cbffd7215a0b6c72d829226cbf86dc585c8
Jane Pulman <jane.pulman@liverpool.ac.uk>
2016-09-08 09:55:43 +0000

    moving phenotype java class so its no longer custom
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@76573 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/Phenotype.java
@@ -0,0 +1,25 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class Phenotype extends DatasetInjector {
+
+  @Override
+  public void injectTemplates() {
+  }
+
+  @Override
+  public void addModelReferences() {
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByPhenotype"); 
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "table", "Phenotype"); 
+  }
+
+  // second column is for documentation
+  @Override
+  public String[][] getPropertiesDeclaration() {
+    String[][] propertiesDeclaration = {};
+    return propertiesDeclaration;
+  }
+
+
+}
