cec9ec97b65938499f40b7aca0b5dce2c705e36d
John Brestelli <jbrestell@gmail.com>
2016-09-14 01:45:00 +0000

    add model refs for crispr question
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@76685 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/ToxoDB/PhenotypeCrispr.java
@@ -0,0 +1,24 @@
+package org.apidb.apicommon.model.datasetInjector.custom.ToxoDB;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class PhenotypeCrispr extends DatasetInjector {
+
+  @Override
+  public void injectTemplates() {
+  }
+
+  @Override
+  public void addModelReferences() {
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByPhenotype_tgonGT1_crisprPhenotype_CrisprScreen_RSRC"); 
+  }
+
+  // second column is for documentation
+  @Override
+  public String[][] getPropertiesDeclaration() {
+    String[][] propertiesDeclaration = {};
+    return propertiesDeclaration;
+  }
+
+
+}
