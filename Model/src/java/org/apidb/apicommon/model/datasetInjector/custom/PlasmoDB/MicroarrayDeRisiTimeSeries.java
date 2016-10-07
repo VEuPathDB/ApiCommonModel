9f1e47f9862a1469a9b1fbe7c2059f61322a63a1
John Brestelli <jbrestell@gmail.com>
2016-10-07 04:31:39 +0000

    no injection for derisi questions
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@77437 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/MicroarrayDeRisiTimeSeries.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/MicroarrayDeRisiTimeSeries.java
@@ -4,10 +4,27 @@ import org.apidb.apicommon.model.datasetInjector.MicroarrayTwoChannelReferenceDe
 
 public class MicroarrayDeRisiTimeSeries extends MicroarrayTwoChannelReferenceDesign {
 
+    @Override
+    public void injectTemplates() {
+      super.injectTemplates();
+
+      // Questions are hard coded in the model w/ the same name that would have been injected
+
+      // we are setting hasPercentile to false so must inject these 
+      setPropValue("graphTextAttrName", "pctGraphAttr" + getDatasetName() + "_pct_graph");
+      injectTemplate("expressionGraphAttributesPercentile");
+      injectTemplate("graphTextAttributeCategory");
+
+    }
+
   @Override
   public void addModelReferences() {
       super.addModelReferences();
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByProfileSimilarity"); 
+
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByMicroarraypfal3D7_microarrayExpression_Derisi_HB3_TimeSeries_RSRC"); 
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByMicroarraypfal3D7_microarrayExpression_Derisi_HB3_TimeSeries_RSRCPercentile"); 
+
   }
 
 
