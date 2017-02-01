cb012abccd148707d33f7f323ea92a75e6986c14
Ryan Doherty <tech@conical.org>
2017-02-01 19:48:16 +0000

    Fix whitespace; also bail if OntologyManipulator cannot load ontology (prevents NPE)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@79585 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/ontology/OntologyMerger.java
+++ Model/src/java/org/apidb/apicommon/model/ontology/OntologyMerger.java
@@ -3,6 +3,7 @@ package org.apidb.apicommon.model.ontology;
 import java.io.File;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -15,59 +16,66 @@ import org.semanticweb.owlapi.model.RemoveImport;
 import org.semanticweb.owlapi.util.AutoIRIMapper;
 
 /**
- * 	Merge all imported OWL files into main OWL file
+ * Merge all imported OWL files into main OWL file
  *
- *  @author Jie Zheng
+ * @author Jie Zheng
  */
 public class OntologyMerger {
-	public static void main(String[] args) {
-		OntologyMergerOptions bean = new OntologyMergerOptions();
-	    CmdLineParser parser = new CmdLineParser(bean);
 
-	    try {
-	        parser.parseArgument(args);
-	    } catch( CmdLineException e ) {
-	        System.err.println(e.getMessage());
-	        parser.printUsage(System.err);
-	        System.exit(1);
-	    }
+  private static final Logger LOG = Logger.getLogger(OntologyMerger.class);
 
-		String path = bean.getPath();
-		String inputFilename = path + bean.getInputFilename();
-		String outputFilename = path + bean.getOutputFilename();
-		//String ontoIRIstr = bean.getOntoIRIstr();
+  public static void main(String[] args) {
+    OntologyMergerOptions bean = new OntologyMergerOptions();
+    CmdLineParser parser = new CmdLineParser(bean);
 
-	    // Get hold of an ontology manager
-        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+    try {
+      parser.parseArgument(args);
+    }
+    catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.exit(1);
+    }
 
-    	AutoIRIMapper mapper = new AutoIRIMapper(new File(path), false);
-    	manager.addIRIMapper(mapper);
+    String path = bean.getPath();
+    String inputFilename = path + bean.getInputFilename();
+    String outputFilename = path + bean.getOutputFilename();
+    // String ontoIRIstr = bean.getOntoIRIstr();
 
-    	// load ontology
-        OWLOntology ont = OntologyManipulator.load(inputFilename, manager);
+    // Get hold of an ontology manager
+    OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 
-        OWLDataFactory df = manager.getOWLDataFactory();
+    AutoIRIMapper mapper = new AutoIRIMapper(new File(path), false);
+    manager.addIRIMapper(mapper);
 
-        // get all imported ontologies
-      	Set<OWLOntology> importOnts = ont.getImports();
-      	Set<OWLImportsDeclaration> importSts = ont.getImportsDeclarations();
+    // load ontology
+    OWLOntology ont = OntologyManipulator.load(inputFilename, manager);
+    if (ont == null) {
+      LOG.warn("Could not load ontology using file: " + inputFilename + " and IRI mapper built with " + path);
+      System.exit(1);
+    }
 
-      	// remove imports statements from the loaded ontology
-     	for(OWLImportsDeclaration importSt: importSts) {
-			IRI importOntIRI = importSt.getIRI();
-			System.out.println(importOntIRI.toString());
-     		RemoveImport ri = new RemoveImport(ont, df.getOWLImportsDeclaration(importOntIRI));
-    		manager.applyChange(ri);
-     	}
+    OWLDataFactory df = manager.getOWLDataFactory();
 
-     	// merge the removed imported ontologies to the loaded one
-     	for(OWLOntology importOnt: importOnts) {
-     		// IRI importOntIRI = importOnt.getOntologyID().getOntologyIRI();
-     		// OntologyManipulator.printPrefixNSs(manager, importOnt);
-    		ont = OntologyManipulator.mergeToTargetOnt(manager, ont, importOnt);
-     	}
+    // get all imported ontologies
+    Set<OWLOntology> importOnts = ont.getImports();
+    Set<OWLImportsDeclaration> importSts = ont.getImportsDeclarations();
 
-     	OntologyManipulator.saveToFile(manager, ont, outputFilename);
-	}
+    // remove imports statements from the loaded ontology
+    for (OWLImportsDeclaration importSt : importSts) {
+      IRI importOntIRI = importSt.getIRI();
+      System.out.println(importOntIRI.toString());
+      RemoveImport ri = new RemoveImport(ont, df.getOWLImportsDeclaration(importOntIRI));
+      manager.applyChange(ri);
+    }
+
+    // merge the removed imported ontologies to the loaded one
+    for (OWLOntology importOnt : importOnts) {
+      // IRI importOntIRI = importOnt.getOntologyID().getOntologyIRI();
+      // OntologyManipulator.printPrefixNSs(manager, importOnt);
+      ont = OntologyManipulator.mergeToTargetOnt(manager, ont, importOnt);
+    }
+
+    OntologyManipulator.saveToFile(manager, ont, outputFilename);
+  }
 }
-
