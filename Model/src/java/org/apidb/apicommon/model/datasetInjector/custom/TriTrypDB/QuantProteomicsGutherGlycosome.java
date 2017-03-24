0f424b9d611db7d5bab61dd2fc9a4ebd70ed56a7
Bindu Gajria <bindu.gajria@gmail.com>
2017-03-24 21:39:54 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonShared/trunk@80627 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/TriTrypDB/QuantProteomicsGutherGlycosome.java
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/custom/TriTrypDB/QuantProteomicsGutherGlycosome.java
@@ -1,25 +1,14 @@
 package org.apidb.apicommon.model.datasetInjector.custom.TriTrypDB;
 
-import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+import org.apidb.apicommon.model.datasetInjector.CusomGenePageExpressionGraphs;
 
-public class QuantProteomicsGutherGlycosome extends DatasetInjector {
-
-  @Override
-  public void injectTemplates() {
-  }
+public class QuantProteomicsGutherGlycosome extends CusomGenePageExpressionGraphs {
 
   @Override
   public void addModelReferences() {
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "profile_graph", "Guther::GlycosomeProteome"); 
 
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesInGlycosomeProteome"); 
-  }
-
-  // second column is for documentation
-  @Override
-  public String[][] getPropertiesDeclaration() {
-    String[][] propertiesDeclaration = {};
-    return propertiesDeclaration;
   }
 
 
