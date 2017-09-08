4fc248b0bf01c88625e52b62e1ddcb8b6b71cdfc
Bindu Gajria <bindu.gajria@gmail.com>
2017-09-08 01:00:02 +0000

    fix dependency
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@84491 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/Popset.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/Popset.java
@@ -2,7 +2,7 @@ package org.apidb.apicommon.model.datasetInjector;
 
 import org.apidb.apicommon.datasetPresenter.DatasetInjector;
 
-public class Popset extends DatasetInjector {
+public class Popset extends SnpChip {
 
   @Override
   public void injectTemplates() {
@@ -11,13 +11,8 @@ public class Popset extends DatasetInjector {
   @Override
   public void addModelReferences() {
 
-      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesBySnps");
-
-      addWdkReference("SnpChipRecordClasses.SnpChipRecordClass", "question", "SnpChipQuestions.SnpBySourceId");
-      addWdkReference("SnpChipRecordClasses.SnpChipRecordClass", "question", "SnpChipQuestions.SnpsByGeneId");
-      addWdkReference("SnpChipRecordClasses.SnpChipRecordClass", "question", "SnpChipQuestions.SnpsByLocation");
-      addWdkReference("SnpChipRecordClasses.SnpChipRecordClass", "question", "SnpChipQuestions.SnpsByStrain");
-      addWdkReference("SnpChipRecordClasses.SnpChipRecordClass", "question", "SnpChipQuestions.SnpsByIsolatePattern");
+      // add all references from SnpChip first
+      super.addModelReferences();
 
       addWdkReference("PopsetRecordClasses.PopsetRecordClass", "question", "PopsetQuestions.PopsetByPopsetId");
       addWdkReference("PopsetRecordClasses.PopsetRecordClass", "question", "PopsetQuestions.PopsetByTaxon");
