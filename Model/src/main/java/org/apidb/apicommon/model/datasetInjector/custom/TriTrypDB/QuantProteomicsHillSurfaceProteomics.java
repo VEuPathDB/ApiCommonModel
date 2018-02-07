d38dcf7f2e746c6863fa3a8e61d64851b53d684c
Bindu Gajria <bindu.gajria@gmail.com>
2018-02-07 15:56:15 +0000

    fix injector
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@87066 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/TriTrypDB/QuantProteomicsHillSurfaceProteomics.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/TriTrypDB/QuantProteomicsHillSurfaceProteomics.java
@@ -5,8 +5,24 @@ import org.apidb.apicommon.model.datasetInjector.QuantitativeProteomicsDirectCom
 public class QuantProteomicsHillSurfaceProteomics extends QuantitativeProteomicsDirectComparison {
 
   @Override
+  public void injectTemplates() {
+      super.injectTemplates();
+  }
+
+  @Override
   public void addModelReferences() {
+      //  add all references from ProteinExpressionMassSpec first
+      super.addModelReferences();
+
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByProteomicsDirectConfidencetbruTREU927_quantitativeMassSpec_BSF_PCF_Surface_Proteomics_RSRC"); 
+
+
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByProteomicsDirecttbruTREU927_quantitativeMassSpec_BSF_PCF_Surface_Proteomics_RSRC"); 
+
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "profile_graph", "Proteomics::LogRatio"); 
+
+	
+
   }
 
 
