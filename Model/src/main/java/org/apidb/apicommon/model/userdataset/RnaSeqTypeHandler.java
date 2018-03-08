d912804b19c58a7d8eea175d740e08468b7d1f4c
Cris Lawrence <crisl@upenn.edu>
2018-03-08 16:22:47 +0000

    Setting up handler display method for type
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@87647 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/userdataset/RnaSeqTypeHandler.java
+++ Model/src/main/java/org/apidb/apicommon/model/userdataset/RnaSeqTypeHandler.java
@@ -46,6 +46,10 @@ import org.json.JSONObject;
  */
 public class RnaSeqTypeHandler extends UserDatasetTypeHandler {
 	
+  public final static String NAME = "RnaSeq";
+  public final static String VERSION = "1.0";
+  public final static String DISPLAY = "RNASeq";
+	
   private static final int WINDOW = 200000;
   
   /**
@@ -90,6 +94,11 @@ public class RnaSeqTypeHandler extends UserDatasetTypeHandler {
   public UserDatasetType getUserDatasetType() {
     // TODO Auto-generated method stub
     return UserDatasetTypeFactory.getUserDatasetType("RnaSeq", "1.0");
+  }
+  
+  @Override
+  public String getDisplay() {
+	return DISPLAY;
   }
 
   //TODO For now there is no data file to install in the DB so no command is needed.
