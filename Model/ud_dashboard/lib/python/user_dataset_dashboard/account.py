8fbc9b7d7055330bf2ffc3d8f5f0b0efd5439cfd
Cris Lawrence <crisl@upenn.edu>
2018-05-17 18:09:53 +0000

    More clean up
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@89084 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/ud_dashboard/lib/python/user_dataset_dashboard/account.py
+++ Model/ud_dashboard/lib/python/user_dataset_dashboard/account.py
@@ -14,11 +14,12 @@ class Account:
 
     def get_users_by_user_ids(self, user_ids):
         """
-        Query to obtain user information (full name and email) for a  given list of user wdk ids.  Needs improvement
-        to handle errors without dangling connections/cursors
+        Query to obtain user information (full name and email) for a  given list of user wdk ids.
         :param user_ids: list of user wdk ids
-        :return: 
+        :return: id, full name and email data for each user
         """
+        # TODO: Becomes inefficient with too many users.  Will need a better strategy.  Also can't use
+        # bind variables with an IN clause.
         self.db.connect()
         sql = """
                    SELECT user_id, first_name | | ' ' | | last_name AS full_name, email
@@ -40,30 +41,5 @@ class Account:
             self.db.execute(sql)
             results = self.db.cursor.fetchall()
             return results
-        finally:
-            self.db.disconnect()
-
-    def get_user_by_id(self, user_id):
-        self.db.connect()
-        sql = """
-          SELECT first_name | | ' ' | | last_name | | ' (' | | email | | ')' AS user_info
-          FROM (
-            SELECT
-              a.user_id, first_name, last_name, email
-            FROM userAccounts.accounts a
-            LEFT JOIN (
-              SELECT
-                user_id,
-                MAX(CASE WHEN key = 'first_name' THEN value END) AS first_name,
-                MAX(CASE WHEN key = 'last_name' THEN value END) AS last_name
-              FROM UserAccounts.Account_Properties
-              GROUP BY user_id) ap
-              ON a.user_id = ap.user_id
-            )
-          WHERE user_id = :user_id"""
-        bindvars = {'user_id': user_id}
-        try:
-            self.db.execute(sql, bindvars=bindvars)
-            return self.db.cursor.fetchone()[0]
         finally:
             self.db.disconnect()
