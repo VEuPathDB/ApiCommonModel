67ded95fb7eb4950ed5aa1b53b94716c91de7025
Bindu Gajria <bindu.gajria@gmail.com>
2018-07-09 19:27:42 +0000

    fix the ontology and add the correct wdkReference for the QuantProt Q in Tryp refs #30673
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@89980 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/TriTrypDB/QuantProteomicsHillSurfaceProteomics.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/TriTrypDB/QuantProteomicsHillSurfaceProteomics.java
@@ -12,7 +12,7 @@ public class QuantProteomicsHillSurfaceProteomics extends QuantitativeProteomics
   @Override
   public void addModelReferences() {
 
-      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByProteomicsDirectConfidencetbruTREU927_quantitativeMassSpec_BSF_PCF_Surface_Proteomics_RSRC"); 
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByProteomicsDirectPValuetbruTREU927_quantitativeMassSpec_BSF_PCF_Surface_Proteomics_RSRC"); 
 
 
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "profile_graph", "Proteomics::LogRatio"); 
