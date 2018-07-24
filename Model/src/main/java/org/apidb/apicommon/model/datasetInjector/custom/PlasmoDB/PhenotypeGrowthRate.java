88b55d2d4517a9772b3ecc25bf7f92a92eede542
John Brestelli <jbrestell@gmail.com>
2018-07-24 20:51:30 +0000

    custom plasmo phenotype injectors
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@90320 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/PhenotypeGrowthRate.java
@@ -0,0 +1,26 @@
+package org.apidb.apicommon.model.datasetInjector.custom.PlasmoDB;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class PhenotypeGrowthRate extends DatasetInjector {
+
+  @Override
+  public void injectTemplates() {
+  }
+
+  @Override
+  public void addModelReferences() {
+      String datasetName = getDatasetName();
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "table", datasetName + "_Phenotype");
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GenesByPhenotype_pberANKA_phenotype_Bushnell_functional_profiling_RSRC");
+  }
+
+  // second column is for documentation
+  @Override
+  public String[][] getPropertiesDeclaration() {
+    String[][] propertiesDeclaration = {};
+    return propertiesDeclaration;
+  }
+
+  
+}
---
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/PhenotypePiggyBac.java
@@ -0,0 +1,25 @@
+package org.apidb.apicommon.model.datasetInjector.custom.PlasmoDB;
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class PhenotypePiggyBac extends DatasetInjector {
+
+  @Override
+  public void injectTemplates() {
+  }
+
+  @Override
+  public void addModelReferences() {
+      String datasetName = getDatasetName();
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "question", "GenesByPhenotype_pfal3D7_phenotype_pB_mutagenesis_MIS_MFS_RSRC");
+  }
+
+  // second column is for documentation
+  @Override
+  public String[][] getPropertiesDeclaration() {
+    String[][] propertiesDeclaration = {};
+    return propertiesDeclaration;
+  }
+
+  
+}
