914a7b7019227849cf80fb5018fb37103b9400a8
Bindu Gajria <bindu.gajria@gmail.com>
2019-02-01 19:27:45 +0000

    add wdkRef for GenesByPhenotype_phenotype_QIAGEN_RSRC Q refs #33350
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@94979 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/FungiDB/PhenotypeCryptococcusInglis.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/FungiDB/PhenotypeCryptococcusInglis.java
@@ -11,6 +11,7 @@ public class PhenotypeCryptococcusInglis extends DatasetInjector {
 
   @Override
   public void addModelReferences() {
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByPhenotype_phenotype_QIAGEN_RSRC"); 
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByTextSearchPhenotype");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "table", "Phenotype"); 
   }
