190c418d413df37bf76beb30f620292f1d761777
Bindu Gajria <bindu.gajria@gmail.com>
2019-02-21 19:00:47 +0000

    changing the hard_floor value for the GenesByProteomicspfal3D7_quantitativeMassSpec_Apicoplast_ER_RSRC Q - refs #32131
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ApiCommonModel/trunk@95474 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/QuantMassSpecApicoplastEr.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/PlasmoDB/QuantMassSpecApicoplastEr.java
@@ -7,7 +7,7 @@ public class QuantMassSpecApicoplastEr extends QuantitativeProteomicsNonRatio {
 
   @Override
   protected void injectTemplate(String templateName) {
-      setPropValue("datasetFloor","0.001");
+      setPropValue("datasetFloor","0.0000001");
       setPropValue("defaultFoldDifference","0.7");
       super.injectTemplate(templateName);
   }
