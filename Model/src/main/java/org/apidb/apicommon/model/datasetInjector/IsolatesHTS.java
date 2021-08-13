2980a8073984f42309c7a901fccc459bd4f8f132
Bindu Gajria <bindu@ash.pcbi.upenn.edu>
2021-08-13 14:31:12 -0400

    add wdk refs for jbrowse track of snps  - refs #44647

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
@@ -79,6 +79,7 @@ public class IsolatesHTS extends DatasetInjector {
       addWdkReference("SnpRecordClasses.SnpRecordClass", "table", "CountrySummary");
       addWdkReference("SnpRecordClasses.SnpRecordClass", "table", "StrainsSamples");
 
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "SnpsGbrowseUrl");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "total_hts_snps");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "hts_nonsynonymous_snps");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "hts_synonymous_snps");
