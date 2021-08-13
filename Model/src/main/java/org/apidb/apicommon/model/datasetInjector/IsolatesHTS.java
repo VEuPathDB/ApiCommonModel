bcdf2575759c3f9a1caa39452f8687560641e3f5
Ryan Doherty <rdoherty@upenn.edu>
2021-08-13 14:19:58 -0400

    Add missing semi-colon

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/IsolatesHTS.java
@@ -84,7 +84,7 @@ public class IsolatesHTS extends DatasetInjector {
       addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "hts_synonymous_snps");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "hts_noncoding_snps");
       addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "hts_stop_codon_snps");
-      addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "hts_nonsyn_syn_ratio")
+      addWdkReference("GeneRecordClasses.GeneRecordClass", "attribute", "hts_nonsyn_syn_ratio");
 
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByNgsSnps");
       //addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByTajimasDHtsSnps");
