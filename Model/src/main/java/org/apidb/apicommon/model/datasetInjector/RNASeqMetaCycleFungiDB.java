30a4aae3b11e685374982c554f3be5afc7e1dcf8
Ryan Doherty <rdoherty@upenn.edu>
2021-08-14 09:52:43 -0400

    clean up imports

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/RNASeqMetaCycleFungiDB.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/RNASeqMetaCycleFungiDB.java
@@ -1,7 +1,5 @@
 package org.apidb.apicommon.model.datasetInjector;
 
-import org.apidb.apicommon.model.datasetInjector.RNASeq;
-
 public class RNASeqMetaCycleFungiDB extends RNASeq {
 
   @Override
@@ -11,7 +9,5 @@ public class RNASeqMetaCycleFungiDB extends RNASeq {
 
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByRNASeqMetaCyclencraOR74A_Bharath_Circadian_Time_Course_ebi_rnaSeq_RSRC"); 
   }
-
-
 
 }
