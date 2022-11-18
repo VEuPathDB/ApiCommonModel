11b457e6c47573ba9de29a1b603783e574fc8dc7
Ryan Doherty <tech@conical.org>
2022-11-18 10:33:06 -0500

    dep upgrades and compatibility

--- Model/src/main/java/org/apidb/apicommon/model/maintain/users5/CutoffDateAware.java
+++
@@ -1,8 +0,0 @@
-package org.apidb.apicommon.model.maintain.users5;
-
-import java.util.Date;
-
-public interface CutoffDateAware {
-
-  void setCutoffDate(Date date);
-}
--- Model/src/main/java/org/apidb/apicommon/model/maintain/users5/mapper/DeleteUsers3Mapper.java
+++
@@ -1,5 +0,0 @@
-package org.apidb.apicommon.model.maintain.users5.mapper;
-
-public interface DeleteUsers3Mapper extends DeleteUsersMapper {
-
-}
