e638fb68ac382b597308e245c130d4a7e95440ef
Ryan Doherty <tech@conical.org>
2022-11-18 10:33:06 -0500

    dep upgrades and compatibility

--- Model/src/main/java/org/apidb/apicommon/model/maintain/users5/ModelAware.java
+++
@@ -1,8 +0,0 @@
-package org.apidb.apicommon.model.maintain.users5;
-
-import org.gusdb.wdk.model.WdkModel;
-
-public interface ModelAware {
-
-  void setModel(WdkModel wdkModel);
-}
--- Model/src/main/java/org/apidb/apicommon/model/maintain/users5/mapper/DeleteUsers3ArchiveSouthMapper.java
+++
@@ -1,5 +0,0 @@
-package org.apidb.apicommon.model.maintain.users5.mapper;
-
-public interface DeleteUsers3ArchiveSouthMapper extends DeleteUsersMapper {
-
-}
