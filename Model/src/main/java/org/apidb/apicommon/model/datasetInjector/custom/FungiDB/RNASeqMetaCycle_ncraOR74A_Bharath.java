e8cc281ba173b2b8a52d795d35dd0f8b90c8c521
Bindu Gajria <bindu@palm.penn.apidb.org>
2022-11-29 14:26:48 -0500

    injectors for RNASeq MetaCycle datasets - refs #43007

---
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/FungiDB/RNASeqMetaCycle_ncraOR74A_Bharath.java
@@ -0,0 +1,13 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+public class RNASeqMetaCycle_RNASeqMetaCycle_ncraOR74A_Bharath extends RNASeq {
+
+  @Override
+  public void addModelReferences() {
+
+      super.addModelReferences();
+
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByRNASeqMetaCyclencraOR74A_Bharath_Circadian_Time_Course_ebi_rnaSeq_RSRC"); 
+  }
+
+}
---
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/FungiDB/RNASeqMetaCycle_ncraOR74A_Hurley.java
@@ -0,0 +1,13 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+public class RNASeqMetaCycle_ncraOR74A_Hurley extends RNASeq {
+
+  @Override
+  public void addModelReferences() {
+
+      super.addModelReferences();
+
+      addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByRNASeqMetaCyclencraOR74A_Hurley_2014_NC_3_ebi_rnaSeq_RSRC"); 
+  }
+
+}
