3a60e159b6dae3ab00d66e4906c110eebd099013
Bindu Gajria <bindu@palm.penn.apidb.org>
2023-02-09 15:05:26 -0500

    inject template for param  -refs #50441

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/QuantitativeProteomicsDirectPValue.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/QuantitativeProteomicsDirectPValue.java
@@ -10,6 +10,8 @@ public class QuantitativeProteomicsDirectPValue extends QuantitativeProteomicsDi
     super.injectTemplates();
 
     injectTemplate("directComparisonGenericPValueQuestion");
+
+    injectTemplate("directComparisonGenericPValueSamplesParamQuery");
   }
 
 
