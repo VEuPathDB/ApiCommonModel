99d95d66ff3bb442215f1b6a4247ebe4bf022df9
Bindu Gajria <bindu@palm.penn.apidb.org>
2023-02-09 13:34:33 -0500

    fix wdkRefs for  template injector  -refs #50441

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/QuantitativeProteomicsDirectPValue.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/QuantitativeProteomicsDirectPValue.java
@@ -4,13 +4,22 @@ package org.apidb.apicommon.model.datasetInjector;
 public class QuantitativeProteomicsDirectPValue extends QuantitativeProteomicsDirectComparison {
 
 
-    @Override
-    public void injectTemplates() {
+  @Override
+  public void injectTemplates() {
 
-      super.injectTemplates();
+    super.injectTemplates();
 
-      injectTemplate("directComparisonGenericPValueQuestion");
-    }
+    injectTemplate("directComparisonGenericPValueQuestion");
+  }
 
 
+
+  @Override
+  public void addModelReferences() {
+
+    addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByProteomicsDirectPValue" + getDatasetName() );
+
+    addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "profile_graph", "Proteomics::LogRatio"); 
+
+  }
 }
