1bf31c8b64e916003af687db2eda4a1cf62a9809
Paul Wilkinson <pwilkinson@palm.penn.apidb.org>
2023-05-10 08:46:48 -0400

    Added uniqueMultiBigWigTrackConfig

---
+++ Model/lib/perl/JBrowseTrackConfig/uniqueMultiBigWigTrackConfig.pm
@@ -0,0 +1,155 @@
+package ApiCommonModel::Model::JBrowseTrackConfig::uniqueMultiBigWigTrackConfig;
+use base qw(ApiCommonModel::Model::JBrowseTrackConfig::CoverageTrackConfig);
+use strict;
+use warnings;
+
+
+# getters and setters
+sub getKey {$_[0]->{key}}
+sub getSubcategory {$_[0]->{subcategory}}
+sub getAttribution {$_[0]->{attribution}}
+sub getUrlTemp {$_[0]->{url_templates}}
+
+sub setKey {$_[0]->{key} = $_[1]}
+sub setSubcategory {$_[0]->{subcategory} = $_[1]}
+sub setAttribution {$_[0]->{attribution} = $_[1]}
+sub setUrlTemp {$_[0]->{url_templates} = $_[1]}
+
+### To Do: Add getter and setter for urlTemplates
+sub getUrlTemplates{
+	my $self = shift;
+
+
+	return $self->getUrlTemp([0]);
+}
+
+
+sub getJBrowseObject{
+	my $self = shift;
+	my $datasetName = $self->getDatasetName();
+	my $displayName = $self->getDisplayName();
+	my $studyDisplayName = $self->getStudyDisplayName();
+	my $file = $self->getFileName();
+	my $urlTemplates = $self->getUrlTemplates();
+	my $color = $self->getColor();
+	my $label = $self->getLabel();
+	my $covMaxScoreDefault = $self->getCovMaxScoreDefault();
+	my $multiCovScale =  $self->getScale();
+	my $category = $self->getCategory();
+	my $key =$self->getKey();
+        my $subCategory = $self->getSubcategory();
+	my $shortAttribution = $self->getAttribution();
+	my $summary = $self->getSummary();
+#my $summary = $self->getSummary();
+	my $jbrowseObject = {storeClass => "MultiBigWig/Store/SeqFeature/MultiBigWig",
+		urlTemplate => $urlTemplates,
+		showTooltips => JSON::true,
+		yScalePosition => "left",
+		key => $key,
+		label => $datasetName  . " Density - Unique Only PAUL",
+		type => "MultiBigWig/View/Track/MultiWiggle/MultiDensity",
+		category => "$category",
+		min_score => 0,
+		max_score => $covMaxScoreDefault,
+		style => {
+			"pos_color"         => "black",
+                        "neg_color"         => "white",
+		},
+		scale => $multiCovScale,
+		metadata => {
+                        subcategory => $subCategory,
+			dataset => $studyDisplayName,
+                        trackType => "Multi-Density",
+                        attribution => $shortAttribution,
+                        description => $summary,
+			alignment => "unique"
+                },
+		fmtMetaValue_Dataset => "function() { return datasetLinkByDatasetName('${datasetName}', '${studyDisplayName}'); }",
+#                     fmtMetaValue_Description => "function() { return datasetDescription('${summary}', ''); }"
+	};
+
+	return $jbrowseObject;
+}
+
+sub getJBrowse2Object{
+	my $self = shift;
+	my $datasetName = $self->getDatasetName();
+	my $displayName = $self->getDisplayName();
+	my $studyDisplayName = $self->getStudyDisplayName();
+	my $file = $self->getFileName();
+	my $urlTemplates = $self->getUrlTemplates();
+	my $color = $self->getColor();
+	my $label = $self->getLabel();
+	my $covMaxScoreDefault = $self->getCovMaxScoreDefault();
+	my $multiCovScale =  $self->getScale();
+	my $category = $self->getCategory();
+	my $subCategory = $self->getSubcategory();
+	my $shortAttribution = $self->getAttribution();
+        my $summary = $self->getSummary();
+	my $jbrowse2Object = {storeClass => "MultiBigWig/Store/SeqFeature/MultiBigWig",
+		yScalePosition => "left",
+		key => "$studyDisplayName - $displayName Coverage",
+		autoscale => "local",
+		style => {
+			"height" => "40"
+		},
+		urlTemplates => [{
+			url => $urlTemplates,	
+			color => $color,
+			name => $datasetName	
+		}],
+		metadata => {
+			subcategory => $subCategory,
+			dataset => $studyDisplayName,
+			trackType => "Multi-Density",	
+			attribution => $shortAttribution,
+			dataset => $datasetName
+		},
+		label => "$datasetName $label Coverage",
+		type => "MultiBigWig/View/Track/MultiWiggle/MultiXYPlot",
+		category => "$category",
+		showTooltips => "true"
+	};
+
+	return $jbrowse2Object;
+}
+
+sub getApolloObject {
+
+	my $self = shift;
+	my $datasetName = $self->getDatasetName();
+	my $displayName = $self->getDisplayName();
+	my $studyDisplayName = $self->getStudyDisplayName();
+	my $file = $self->getFileName();
+	my $urlTemplates = $self->getUrlTemplates();
+	my $color = $self->getColor();
+	my $label = $self->getLabel();
+	my $covMaxScoreDefault = $self->getCovMaxScoreDefault();
+	my $multiCovScale =  $self->getScale();
+	my $category = $self->getCategory();
+#my $summary = $self->getSummary();
+	my $apolloObject={storeClass => "JBrowse/Store/SeqFeature/BigWig",
+		key => "$studyDisplayName - $displayName Coverage",
+		yScalePosition => "left",
+		urlTemplate => $urlTemplates,		  
+		scale => "log",
+		max_score => $covMaxScoreDefault,
+		metadata => {
+			subcategory => "RNA-Seq",
+			trackType => "Coverage",
+			alignment => "unique",
+			dataset => $datasetName,		  		  
+		},
+		label => "$datasetName $label Coverage",
+		type => "JBrowse/View/Track/Wiggle/XYPlot",
+		min_score => 0,		  
+		max_score => 1000,
+		scale => "log",
+		fmtMetaValue_Dataset => "function() { return datasetLinkByDatasetName('${datasetName}', '${studyDisplayName}'); }",
+#                  fmtMetaValue_Description => "function() { return datasetDescription('${summary}', ''); }"
+	};
+
+	return $apolloObject;
+}
+
+1;
