e6e20450bf69df1b0a1aa62a07f2c76864f54fdb
Paul Wilkinson <pwilkinson@palm.penn.apidb.org>
2023-08-22 05:34:05 -0400

    edit setUrlTemplate and setIndexUrlTemplate methods

--- Model/lib/perl/JBrowseTrackConfig/AlignmentsTrackConfig.pm
+++ Model/lib/perl/JBrowseTrackConfig/AlignmentsTrackConfig.pm
@@ -5,17 +5,47 @@ use strict;
 use warnings;
 
 sub getUrlTemplate {$_[0]->{url_template} }
-sub setUrlTemplate {$_[0]->{url_template} = $_[1]}
+sub setUrlTemplate {
+    my($self, $urlTemplate) = @_;
+    die "required urlTemplate not set" unless $urlTemplate;
+    $self->{url_template} = $urlTemplate;
+}
 
 sub getIndexUrlTemplate {$_[0]->{index_url_template} }
-sub setIndexUrlTemplate {$_[0]->{index_url_template} = $_[1]}
+sub setIndexUrlTemplate {
+    my($self, $urlTemplate) = @_;
+    die "required urlTemplate not set" unless $urlTemplate;
+    $self->{index_url_template} = $urlTemplate;
+}
 
+sub getColor {$_[0]->{color}}
+sub setColor {$_[0]->{color} = $_[1]}
+
+sub getStoreClass {$_[0]->{storeClass}}
+sub setStoreClass {$_[0]->{storeClass} = $_[1]}
+
+sub getMin {$_[0]->{min}}
+sub setMin {$_[0]->{min} = $_[1]}
+
+sub getMax {$_[0]->{max}}
+sub setMax {$_[0]->{max} = $_[1]}
+
+sub getBigwigUrl {$_[0]->{bw_url_template}}
+sub setBigwigUrl {
+    my($self, $bigwigUrl) = @_;
+    die "required bigwigUrl not set" unless $bigwigUrl;
+    $self->{bw_url_template} = $bigwigUrl;
+}
 
 sub new {
     my ($class, $args) = @_;
     my $self = $class->SUPER::new($args);
 
     $self->setUrlTemplate($args->{url_template});
+    $self->setStoreClass($args->{storeClass});
+    $self->setMin($args->{min});
+    $self->setMax($args->{max});
+    $self->setBigwigUrl($args->{bw_url_template});
 
     my $indexLocation = $args->{url_template} . ".bai";
     $self->setIndexUrlTemplate($indexLocation);
@@ -40,11 +70,32 @@ sub new {
     return $self;
 }
 
+
+sub getHistograms {
+    my $self = shift;
+
+    my $bigwigUrl = $self->getBigwigUrl();
+
+    my $histograms = {};
+    $histograms->{color} = "black";
+    $histograms->{storeClass} = "JBrowse/Store/SeqFeature/BigWig";
+    $histograms->{min} = 0;
+    $histograms->{urlTemplate} = $bigwigUrl ;
+    $histograms->{max} = 500;
+    return $histograms;
+}
+
+
 sub getJBrowseObject{
     my $self = shift;
 
     my $jbrowseObject = $self->SUPER::getJBrowseObject();
-    # TODO
+    $jbrowseObject->{yScalePosition}="left";
+    $jbrowseObject->{chunkSizeLimit}=50000000;
+    $jbrowseObject->{urlTemplate}= $self->getUrlTemplate();
+
+    my $histograms = $self->getHistograms();
+    $jbrowseObject->{histograms} = $histograms;
 
     return $jbrowseObject;
 }
