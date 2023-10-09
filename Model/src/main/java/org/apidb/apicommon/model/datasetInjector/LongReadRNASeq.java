6ed546351acbb4d14a402c51237f18550fec823d
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2023-10-09 13:09:07 -0400

    Inject correct model refs for long read data

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/LongReadRNASeq.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/LongReadRNASeq.java
@@ -4,14 +4,27 @@ import org.apidb.apicommon.datasetPresenter.DatasetInjector;
 
 public class LongReadRNASeq extends DatasetInjector {
 
+
+   protected String getInternalQuestionName() {
+        return "GeneQuestions.GenesByLongReadEvidence_" + getDatasetName();
+    }
+
   @Override
   public void injectTemplates() {
     // TODO:  possibly inject jbrowse track??
+    String projectName = getPropValue("projectName");
+    setPropValue("includeProjects", projectName);
+    injectTemplate("LongReadGeneModelQuestion");
+
+
+    setPropValue("questionName", getInternalQuestionName());
+    setPropValue("searchCategory", "searchCategory-longread");
+    injectTemplate("internalGeneSearchCategory");
   }
 
   @Override
   public void addModelReferences() {
-	  addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.LongReadTranscriptCounts");
+	  addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", getInternalQuestionName());
 	  addWdkReference("DynSpanRecordClasses.DynSpanRecordClass", "question", "SpanQuestions.LongReadNovelGeneSpansBySourceId");
 	  //addWdkReference("DynSpanRecordClasses.DynSpanRecordClass", "question", "SpanQuestions.IntronJunctionDynamicSearch");
   }
