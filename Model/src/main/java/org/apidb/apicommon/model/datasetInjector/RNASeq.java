de70ae690c5c718596888b54e4af6d1765c53f80
Kathryn Crouch <kathryn.crouch@glasgow.ac.uk>
2023-10-24 08:37:21 -0400

    Don't inject model ref for intron span question for projects where we exclude this question

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/RNASeq.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/RNASeq.java
@@ -384,8 +384,9 @@ public class RNASeq extends  DatasetInjector {
           addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question",
                           "GeneQuestions.GenesByRNASeq" + getDatasetName() + "Percentile");
 
-          //addWdkReference("GeneRecordClasses.GeneRecordClass", "question","DatasetQuestions.DatasetsByReferenceName");
-          if (getPropValueAsBoolean("showIntronJunctions")) {
+          // this question does not display on Host or Trich
+          String projectName = getPropValue("projectName");
+          if (getPropValueAsBoolean("showIntronJunctions") && !projectName.equals("HostDB") && !projectName.equals("TrichDB")) {
           	addWdkReference("DynSpanRecordClasses.DynSpanRecordClass", "question", "SpanQuestions.IntronJunctionDynamicSearch");
           }
 
