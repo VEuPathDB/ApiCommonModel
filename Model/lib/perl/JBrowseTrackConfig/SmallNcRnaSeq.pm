6360976d9bbf028c716e96ff5785b677078c8527
Paul Wilkinson <pwilkinson@palm.penn.apidb.org>
2023-11-14 08:34:41 -0500

    Added new subclasses

---
+++ Model/lib/perl/JBrowseTrackConfig/SmallNcRnaSeq.pm
@@ -0,0 +1,77 @@
+package ApiCommonModel::Model::JBrowseTrackConfig::SmallNcRnaSeq;
+use base qw(ApiCommonModel::Model::JBrowseTrackConfig::TrackConfig);
+
+use strict;
+use warnings;
+
+use ApiCommonModel::Model::JBrowseTrackConfig::BamStore;
+
+sub getYScalePosition {$_[0]->{yscale_position}}
+sub setYScalePosition {$_[0]->{yscale_position} = $_[1]}
+
+sub getBorderColor {$_[0]->{border_color}}
+sub setBorderColor {$_[0]->{border_color} = $_[1]}
+
+sub getUrlTemplate {$_[0]->{url_template} }
+sub setUrlTemplate {$_[0]->{url_template} = $_[1]}
+
+
+sub new {
+    my ($class, $args) = @_;
+    my $self = $class->SUPER::new($args);
+
+    $self->setId("Small Non-coding RNAs");
+    $self->setLabel("Small Non-coding RNAs");
+
+    my $store = ApiCommonModel::Model::JBrowseTrackConfig::BamStore->new($args);
+    $self->setStore($store);
+
+    if($self->getApplicationType() eq 'jbrowse' || $self->getApplicationType() eq 'apollo') {
+        $self->setDisplayType("SmallRNAPlugin/View/Track/smAlignments");
+        $self->setTrackTypeDisplay("Small RNA");
+    }
+    else {
+        $self->setTrackType("AlignmentsTrack");
+        $self->setDisplayType("LinearAlignmentsDisplay");
+        $self->setTrackTypeDisplay("Alignment");
+    }
+
+    my $datasetConfig = $self->getDatasetConfigObj();
+    $datasetConfig->setCategory("Transcriptomics");
+    $datasetConfig->setSubcategory("small non-coding RNA");
+
+    $self->setYScalePosition("left");
+    $self->setUrlTemplate($args->{url_template});
+    return $self;
+}
+
+sub getJBrowseObject{
+    my $self = shift;
+
+    my $jbrowseObject = $self->SUPER::getJBrowseObject();
+
+    $jbrowseObject->{unsafePopup} = "JSON::true";
+    $jbrowseObject->{yScalePosition} = $self->getYScalePosition();
+    $jbrowseObject->{url_template}= $self->getUrlTemplate();
+
+    return $jbrowseObject;
+}
+
+sub getJBrowse2Object{
+    my $self = shift;
+
+    my $jbrowse2Object = $self->SUPER::getJBrowse2Object();
+
+    my $indexLocation = $self->getIndexUrlTemplate();
+
+    $jbrowse2Object->{adapter}->{index}->{location}->{uri} = $indexLocation;
+    $jbrowse2Object->{adapter}->{bamLocation} = {uri => $self->getUrlTemplate(),locationType => "UriLocation"};
+
+    $jbrowse2Object->{displays}->[0]->{displayId} = "bam_" . scalar($self);
+
+    return $jbrowse2Object;
+}
+
+
+
+1;
