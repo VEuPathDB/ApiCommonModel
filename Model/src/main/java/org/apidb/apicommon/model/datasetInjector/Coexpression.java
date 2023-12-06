4be5ec703f4c2ab7bd577d5a72ff39626074121d
Ryan Doherty <tech@conical.org>
2023-12-05 22:55:22 -0500

    Fix java compiler warnings and whitespace

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/Coexpression.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/Coexpression.java
@@ -1,7 +1,6 @@
 package org.apidb.apicommon.model.datasetInjector;
 
 import org.apidb.apicommon.datasetPresenter.DatasetInjector;
-import org.gusdb.wdk.model.WdkRuntimeException;
 
 public class Coexpression extends  DatasetInjector {
 
@@ -9,15 +8,12 @@ public class Coexpression extends  DatasetInjector {
   public void injectTemplates() {
       setShortAttribution();
 
-      String datasetName = getDatasetName();
+      //String datasetName = getDatasetName();
 
       String projectName = getPropValue("projectName");
-
-      String dataSource  = getPropValue("dataSource");
-      String exampleGeneIds  = getPropValue("exampleGeneIds");
-      String defaultCoefficient = getPropValue("defaultCoefficient");
-
-
+      //String dataSource  = getPropValue("dataSource");
+      //String exampleGeneIds  = getPropValue("exampleGeneIds");
+      //String defaultCoefficient = getPropValue("defaultCoefficient");
 
       if(getPropValueAsBoolean("isEuPathDBSite")) {
           setPropValue("includeProjects", projectName + ",EuPathDB,UniDB");
@@ -26,13 +22,11 @@ public class Coexpression extends  DatasetInjector {
       }
       setPropValue("includeProjectsExcludeEuPathDB", projectName + ",UniDB");
 
-
       injectTemplate("datasetCategory");
 
       setPropValue("searchCategory", "searchCategory-coexpression");
       setPropValue("questionName", "GeneQuestions.GenesByCoexpression" + getDatasetName());
       injectTemplate("internalGeneSearchCategory");
-
 
       injectTemplate("coexpressionQuestion");
       injectTemplate("coexpressionSourceQuery");
@@ -41,11 +35,11 @@ public class Coexpression extends  DatasetInjector {
   @Override
   public void addModelReferences() {
       addWdkReference("TranscriptRecordClasses.TranscriptRecordClass", "question", "GeneQuestions.GenesByCoexpression" + getDatasetName());
-
   }
 
   // declare properties required beyond those inherited from the datasetPresenter
   // second column is for documentation
+  @Override
   public String[][] getPropertiesDeclaration() {
     String [][] propertiesDeclaration = { {"dataSource", "data source in the coexpression table"},
                                           {"exampleGeneIds", "Gene Ids provided as default on coexpression Q page"},
