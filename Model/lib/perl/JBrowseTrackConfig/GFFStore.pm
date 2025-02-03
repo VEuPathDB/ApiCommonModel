9e34810c4ded82bdf4cb34d5fe83350e3a86c361
Paul Wilkinson <pwilkinson@palm.penn.apidb.org>
2025-02-03 12:58:00 -0500

    Edited Store classes and Track Config for Pbrowse tracks

--- Model/lib/perl/JBrowseTrackConfig/GFFStore.pm
+++ Model/lib/perl/JBrowseTrackConfig/GFFStore.pm
@@ -3,13 +3,23 @@ use base qw(ApiCommonModel::Model::JBrowseTrackConfig::Store);
 use strict;
 use warnings;
 
+sub getBaseUrl {$_[0]->{base_url}}
+sub setBaseUrl {$_[0]->{base_url} = $_[1] }
+
 sub getUrlTemplate {$_[0]->{url_template} }
 sub setUrlTemplate {$_[0]->{url_template} = $_[1]}
+
+sub getQuery {$_[0]->{query}}
+sub setQuery {$_[0]->{query} = $_[1] }
+
+sub getQueryParamsHash {$_[0]->{query_params_hash}}
+sub setQueryParamsHash {$_[0]->{query_params_hash} = $_[1] }
 
 sub new {
     my ($class, $args) = @_;
     my $self = $class->SUPER::new($args);
 
+    $self->setBaseUrl("/a/service/jbrowse");
     $self->setUrlTemplate($args->{url_template});
 
     if($self->getApplicationType() eq 'jbrowse' || $self->getApplicationType() eq 'apollo') {
