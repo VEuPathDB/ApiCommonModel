5ac70975af5e4e08166c931e4fb62c0d90ceaea2
bindu <bindu@palm.penn.apidb.org>
2025-07-14 14:53:52 -0400

    allow injectTemplates for a couple of datasets

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/RNASeq.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/RNASeq.java
@@ -54,10 +54,11 @@ public class RNASeq extends  DatasetInjector {
       String projectName = getPropValue("projectName");
       //String presenterId = getPropValue("presenterId");
       String datasetName = getDatasetName();
-      
-      // TODO - remove this condition, to add templates for specific dataset(s) 
-      if (!datasetName.equals("pfal3D7_Newbold_ebi_rnaSeq_RSRC")) { 
-	  return;
+
+      // TODO - remove this condition, to add templates for specific dataset(s)
+      if (!(datasetName.equals("pfal3D7_Newbold_ebi_rnaSeq_RSRC") ||
+						datasetName.equals("pfal3D7_Duffy_rnaSeq_RSRC"))) {
+					return;
       }
       Boolean switchStrandsGBrowse = getPropValueAsBoolean("switchStrandsGBrowse");
       Boolean switchStrandsProfiles = getPropValueAsBoolean("switchStrandsProfiles");
