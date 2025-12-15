be7a04e6afde910a7a1b812962f837383dd868bd
Paul Wilkinson <pwilkinson@palm.penn.apidb.org>
2025-12-15 11:20:29 -0500

    Fixed url for dnaseq tracks generating a track error

--- Model/lib/perl/JBrowseTrackConfig/AlignmentsTrackConfig.pm
+++ Model/lib/perl/JBrowseTrackConfig/AlignmentsTrackConfig.pm
@@ -55,7 +55,9 @@ sub new {
     $self->setMin(0);
     $self->setMax(500);
     $self->setYScalePosition("left");
-    $self->setChunkSizeLimit(50000000);
+    #$self->setChunkSizeLimit(50000000);
+    $self->setChunkSizeLimit(2000000000);
+    #$self->fetchSizeLimit(2000000000);
 
     if($args->{skip_histograms}) {
       $self->{_skip_histograms} = 1;
@@ -89,6 +91,8 @@ sub getHistograms {
     my $store = $self->getStore();
 
     my $bigwigUrl = $store->getBigwigUrl();
+    $bigwigUrl = "/a/service/jbrowse/store?data=" . $bigwigUrl;
+
 
     my $histograms = {};
     #$histograms->{color} = $self->getColor();
