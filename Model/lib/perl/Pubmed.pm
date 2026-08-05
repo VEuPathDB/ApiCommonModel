4a85f9903200861db4ae62a30c3aed9b770f24aa
Mustafa Nural <mvnural@gmail.com>
2026-08-05 16:03:56 -0400

    speed up Pubmed.pm.
    - Read XML once instead of using XPath expressions.
    - Add a retry with progressive sleep when we get errors if we are too quick
    - sleep 100ms instead of 300ms as we use the api_key now.

--- Model/lib/perl/Pubmed.pm
+++ Model/lib/perl/Pubmed.pm
@@ -3,10 +3,7 @@ package ApiCommonModel::Model::Pubmed;
 use strict;
 require LWP::UserAgent;
 use XML::LibXML;
-use XML::XPath;
-use XML::XPath::XMLParser;
 use Data::Dumper;
-use Time::HiRes qw(usleep);
 
 sub new {
   my ($class, $apiKey, $pmidList) = @_;
@@ -21,97 +18,132 @@ sub new {
   push(@formdata, "db", "pubmed");
   push(@formdata, "id", join(',', @$pmidList));
   push(@formdata, "api_key", $apiKey);
-  my $response = $ua->post($pubmedUrl, \@formdata);
+  # NCBI intermittently returns 502s and other transient errors, so retry a few
+  # times before giving up on the chunk
+  my $MAXTRIES = 5;
+  my $response;
+  for (my $try = 1; $try <= $MAXTRIES; $try++) {
+    $response = $ua->post($pubmedUrl, \@formdata);
+    last if $response->is_success;
+
+    print STDERR "NCBI request failed (attempt $try of $MAXTRIES): "
+                 . $response->status_line . "\n";
+    sleep($try) # increase sleep over retries.
+      unless ($try == $MAXTRIES);
+  }
   if (!($response->is_success)) {
     die $response->status_line;
   }
 
-  # nap for a third of a second to observe NCBI's three-request-per-second limit
-  usleep(100000);
-
-  # print $response->decoded_content;
-
-  my $xp = XML::XPath->new(xml => $response->decoded_content);
+  # hand libxml2 the undecoded octets so that it honors the encoding declared in
+  # the response; charset => 'none' still undoes any Content-Encoding
+  my $xml = $response->decoded_content(charset => 'none');
+  my $doc = XML::LibXML->load_xml(string => $xml, no_blanks => 1);
 
   my $self = {pmidList => $pmidList,
-              xml => $response->decoded_content,
-	      xp => $xp
+              xml => $xml,
+	      doc => $doc
 	     };
 
   bless($self, $class);
+
+  $self->_indexDocSums();
+
   return $self;
+}
+
+# Walk the DocSum nodes once and index the values we need by PubMed ID.  Doing it
+# here, rather than running an XPath search from the document root inside each
+# getter, keeps the cost linear in the number of records per chunk instead of
+# quadratic.
+sub _indexDocSums {
+  my ($self) = @_;
+
+  my %byPmid;
+
+  foreach my $docSum ($self->{doc}->findnodes('/eSummaryResult/DocSum')) {
+    my ($idNode) = $docSum->getChildrenByTagName('Id');
+    next unless $idNode;
+
+    my $record = {node => $docSum, authors => []};
+
+    foreach my $item ($docSum->getChildrenByTagName('Item')) {
+      my $itemName = $item->getAttribute('Name');
+      next unless defined $itemName;
+
+      if ($itemName eq 'AuthorList') {
+        # take every child Item, as the previous XPath did, so that entries such
+        # as CollectiveName are not dropped
+        push(@{$record->{authors}},
+             map { $_->textContent } $item->getChildrenByTagName('Item'));
+      }
+      elsif ($itemName eq 'DOI' || $itemName eq 'Title' || $itemName eq 'LastAuthor') {
+        $record->{$itemName} = $item->textContent;
+      }
+    }
+
+    $byPmid{$idNode->textContent} = $record;
+  }
+
+  $self->{byPmid} = \%byPmid;
+}
+
+sub _getItem {
+  my ($self, $pmid, $itemName) = @_;
+
+  my $record = $self->{byPmid}->{$pmid};
+
+  return (defined $record && defined $record->{$itemName})
+    ? $record->{$itemName}
+    : '';
+}
+
+sub _getAuthorList {
+  my ($self, $pmid) = @_;
+
+  my $record = $self->{byPmid}->{$pmid};
+
+  return $record ? @{$record->{authors}} : ();
 }
 
 sub getFullRecord {
   my ($self, $pmid) = @_;
 
-  my $searchString = "/eSummaryResult/DocSum[Id='$pmid']";
-  my $nodeset = $self->{xp}->find($searchString);
+  my $record = $self->{byPmid}->{$pmid};
 
-  my $node = $nodeset->get_node(1);
-  return XML::XPath::XMLParser::as_string($node);
+  return $record ? $record->{node}->toString() : '';
 }
 
 sub getDoi {
   my ($self, $pmid) = @_;
 
-  my $searchString = "/eSummaryResult/DocSum[Id='$pmid']/Item[\@Name='DOI']";
-  my $nodeset = $self->{xp}->find($searchString);
-
-#  foreach my $node ($nodeset->get_nodelist) {
-#    print "in loop: " . $node->string_value . "\n";
-#  }
-
-  my $node = $nodeset->get_node(1);
-  return $node ? $node->string_value : '';
-
+  return $self->_getItem($pmid, 'DOI');
 }
 
 sub getTitle {
   my ($self, $pmid) = @_;
 
-  my $searchString = "/eSummaryResult/DocSum[Id='$pmid']/Item[\@Name='Title']";
-  my $nodeset = $self->{xp}->find($searchString);
-
-  my $node = $nodeset->get_node(1);
-  return $node ? $node->string_value : '';
-
+  return $self->_getItem($pmid, 'Title');
 }
 
 sub getLastAuthor {
   my ($self, $pmid) = @_;
 
-  my $searchString = "/eSummaryResult/DocSum[Id='$pmid']/Item[\@Name='LastAuthor']";
-  my $nodeset = $self->{xp}->find($searchString);
-
-  my $node = $nodeset->get_node(1);
-  return $node ? $node->string_value : '';
-
+  return $self->_getItem($pmid, 'LastAuthor');
 }
 
 sub getAuthors {
   my ($self, $pmid) = @_;
 
-  my $searchString = "/eSummaryResult/DocSum[Id='$pmid']/Item[\@Name='AuthorList']/Item";
-  my $nodeset = $self->{xp}->find($searchString);
-
-  my @authors;
-  foreach my $node ($nodeset->get_nodelist) {
-    push(@authors, $node->string_value);
-  }
-
-  return join(', ', @authors);
-
+  return join(', ', $self->_getAuthorList($pmid));
 }
 
 sub getFirstAuthor {
   my ($self, $pmid) = @_;
 
-  my $searchString = "/eSummaryResult/DocSum[Id='$pmid']/Item[\@Name='AuthorList']/Item";
-  my $nodeset = $self->{xp}->find($searchString);
+  my ($firstAuthor) = $self->_getAuthorList($pmid);
 
-  my $node = $nodeset->get_node(1);
-  return $node ? $node->string_value : '';
+  return defined $firstAuthor ? $firstAuthor : '';
 }
 
 1;
